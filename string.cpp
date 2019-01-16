0aa7c1547fc9365faf4da8313444101ac248d231
zegabr <jgsp2@cin.ufpe.br>
2019-01-16 16:22:08 -0300

    z-algorithm added

--- string.cpp
+++ string.cpp
@@ -43,7 +43,6 @@ void kmpPre() {
 		b[++i] = ++j;
 	}
 }
-
 void kmpSearch() {
 	int i = 0, j = 0, ans = 0;
 	while(i < n) {
@@ -58,3 +57,17 @@ void kmpSearch() {
 	return ans;
 }
 
+/////////////Z-ALGORITHM////
+//
+vector<int> z(string s){
+	int n = s.size();
+	vector<int> z(n);
+	int x=0,y=0;
+	for(int i=1;i<n;i++){
+		z[i]=max(0,min(z[i-x],y-i+1));
+		while(i+z[i]<n and s[z[i]]==s[i+z[i]]){
+			x=i; i=i+z[i]; z[i]++;
+		}
+	}
+	return z;//z-array
+}
