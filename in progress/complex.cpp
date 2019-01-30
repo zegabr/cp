7f07cff2b9f6ad1a023277005210b6ba3f07165f
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-01-30 16:43:11 -0300

    playing with geometry

--- in progress/complex.cpp
+++ in progress/complex.cpp
@@ -8,14 +8,45 @@ typedef complex<ll> P;
 #define X real()
 #define Y imag()
 
-long double distanceof2points(P &a, P &b){
-	return abs(b-a);
+
+
+#include<cmath>
+#include<complex>
+typedef complex<double>P;
+#define x real()
+#define y imag()
+#define PI 3.1415926535897932384626
+//abs(P) = size
+//norm(P) = size^2
+//arg(P) = angle in radians
+double angleDegrees(P p){
+	return arg(p)*(180/PI);
 }
-long double anglebetween2points(P &a, P &b){
-return 0;
+P rotation(P p, double radians){
+	return p*polar(1.0,radians);
 }
-ll Xproduct(P &a, P &b){
-	return (conj(a)*b).Y;
+double dotProduct(P p, P q){
+	return (conj(p)*q).x;
+}
+double crossProduct(P p, P q){
+	return abs((conj(p)*q).y);
+}
+P lineintersection(P a,P b,P c,P d){
+	b=b-a; d=c-d; c=c-a;
+	return a+b*crossProduct(c,d)/crossProduct(b,d);
+}
+P circlecenter(P a, P b,P c){
+	b=(a+b)/2.0;
+	c=(a+c)/2.0;
+	return lineintersection(b,b+rotation(a-b,3*PI/2),c,c+rotation(a-c,3*PI/2));
+}
+ld gcd(ld a, ld b){//gcd between doubles
+	ld eps=1e-4;	
+	while(fabs(a) > eps and fabs(b)>eps){
+		if(a>b) a-=floor(a/b)*b;
+		else b-=floor(b/a)*a;
+	}
+	return a+b;
 }
 int main(){
 	
