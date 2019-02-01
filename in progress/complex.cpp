48e5a39a941244c274253824149b15aeb507658a
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-02-01 19:56:08 -0300

    convexhull optimized

--- in progress/complex.cpp
+++ in progress/complex.cpp
@@ -17,10 +17,10 @@ typedef complex<double>P;
 //abs(P) = size
 //norm(P) = size^2
 //arg(P) = angle in radians
-double angleDegrees(P p){
+double angleDegrees(const P &p){
 	return arg(p)*(180/PI);
 }
-P rotate(P p, double radians){
+P rotate(const P &p, const double &radians){
 	return p*polar(1.0,radians);
 }
 double dot(const P &p,const P &q){
@@ -31,7 +31,7 @@ double cross(const P &p,const P &q){
 }
 ld gcd(ld a, ld b){//gcd between doubles(maybe angles)
 	ld eps=1e-4;	
-	while(fabs(a) > eps and fabs(b)>eps){
+	while(fabs(a)>eps and fabs(b)>eps){
 		if(a>b) a-=floor(a/b)*b;
 		else b-=floor(b/a)*a;
 	}
--- math/convexhull.cpp
+++ math/convexhull.cpp
@@ -1,47 +1,41 @@
 #include<complex>
-#include<stack>
 #include<vetor>
 #define x real()
 #define y imag()
-typedef complex<double> P;
+#define PI 3.1415926535897932384626
 
-double cross(P p, P q){
+typedef long double ld;
+typedef complex<ld> P;
+const ld eps=1e-16;
+
+ld cross(const P &p,const P &q){//use long long if possible
 	return (conj(p)*q).y;
 }
 P orig;
-bool comp(const P &a, const P &b){
-	//compare by angle between a-orig and b-orig
-	return arg(a-orig)<arg(b-orig) or (arg(a-orig)-arg(b-orig)<=1e-5 and abs(a-orig)>abs(b-orig) );
+bool comp(const P &a, const P &b){//works on leftmost
+	//compare by angles and distance between a-orig and b-orig
+	ld arg1 = arg(a-orig), arg2 = arg(b-orig), abs1 = abs(a-orig), abs2 = abs(b-orig);
+	if(fabs(arg1-arg2)<eps){
+		if(fabs(arg1-PI/2.0)<eps)return abs1-abs2>eps;
+		return abs1-abs2<eps;
+	}else return arg1<arg2;
 }
-vector<P> convexhull(vector<P> &v){
+void convexhull(vector<P> &v){
 	if(v.size()<3) return v;
-
 	int	ind = 0;
-	for(int i=1;i<v.size();i++){//get bottommost
-		if(v[i].x<v[ind].x or (v[i].x==v[ind].x and v[i].y<v[ind].y))
-			ind=i;
+	for(int i=1;i<v.size();i++){ //get leftmost
+		if(v[i].x<v[ind].x or (v[i].x==v[ind].x and v[i].y<v[ind].y)) ind=i;
 	}
 	orig=v[ind];
 	swap(v[0],v[ind]);
 	sort(v.begin()+1,v.end(),comp);//counterclockwise
-    stack<P> h;
-	P middle, prev;
-	h.push(v[0]); h.push(v[1]);
-	for(int i=2; i<v.size(); i++){
-			middle=h.top(); h.pop();
-			prev=h.top();
-		while(h.size()>=2 and cross(middle-prev, v[i]-middle)<=0){//while turn right
-			middle=h.top(); h.pop();
-			prev=h.top();
+	int s=3;
+	for(int i=3; i<v.size(); i++){
+		while(s>=3 and cross(v[s-1]-v[s-2], v[i]-v[s-1])<=0.0){//while turn right or collinear 
+			s--;
 		}
-		h.push(middle);
-		h.push(v[i]);
-
+		v[s]=v[i];
+		s++;
 	}
-	vector<P> ans(h.size());
-	while(h.size()){
-		ans[h.size()-1]=h.top();
-		h.pop();
-	}
-	return ans;
+	v.resize(s);//hull here
 }
