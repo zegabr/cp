03f79552f66b23e15696c0638572d0fb3b80358e
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-02-01 12:45:42 -0300

    convex hull added

--- in progress/complex.cpp
+++ in progress/complex.cpp
@@ -8,8 +8,6 @@ typedef complex<ll> P;
 #define X real()
 #define Y imag()
 
-
-
 #include<cmath>
 #include<complex>
 typedef complex<double>P;
@@ -22,25 +20,16 @@ typedef complex<double>P;
 double angleDegrees(P p){
 	return arg(p)*(180/PI);
 }
-P rotation(P p, double radians){
+P rotate(P p, double radians){
 	return p*polar(1.0,radians);
 }
-double dotProduct(P p, P q){
+double dot(const P &p,const P &q){
 	return (conj(p)*q).x;
 }
-double crossProduct(P p, P q){
-	return abs((conj(p)*q).y);
+double cross(const P &p,const P &q){
+	return (conj(p)*q).y;
 }
-P lineintersection(P a,P b,P c,P d){
-	b=b-a; d=c-d; c=c-a;
-	return a+b*crossProduct(c,d)/crossProduct(b,d);
-}
-P circlecenter(P a, P b,P c){
-	b=(a+b)/2.0;
-	c=(a+c)/2.0;
-	return lineintersection(b,b+rotation(a-b,3*PI/2),c,c+rotation(a-c,3*PI/2));
-}
-ld gcd(ld a, ld b){//gcd between doubles
+ld gcd(ld a, ld b){//gcd between doubles(maybe angles)
 	ld eps=1e-4;	
 	while(fabs(a) > eps and fabs(b)>eps){
 		if(a>b) a-=floor(a/b)*b;
@@ -48,6 +37,7 @@ ld gcd(ld a, ld b){//gcd between doubles
 	}
 	return a+b;
 }
+
 int main(){
 	
 }
---
+++ math/convexhull.cpp
@@ -0,0 +1,47 @@
+#include<complex>
+#include<stack>
+#include<vetor>
+#define x real()
+#define y imag()
+typedef complex<double> P;
+
+double cross(P p, P q){
+	return (conj(p)*q).y;
+}
+P orig;
+bool comp(const P &a, const P &b){
+	//compare by angle between a-orig and b-orig
+	return arg(a-orig)<arg(b-orig) or (arg(a-orig)-arg(b-orig)<=1e-5 and abs(a-orig)<abs(b-orig) );
+}
+vector<P> convexhull(vector<P> &v){
+	if(v.size()<3) return v;
+
+	int	ind = 0;
+	for(int i=1;i<v.size();i++){//get bottommost
+		if(v[i].x<v[ind].x or (v[i].x==v[ind].x and v[i].y<v[ind].y))
+			ind=i;
+	}
+	orig=v[ind];
+	swap(v[0],v[ind]);
+	sort(v.begin()+1,v.end(),comp);//counterclockwise
+    stack<P> h;
+	P middle, prev;
+	h.push(v[0]); h.push(v[1]);
+	for(int i=2; i<v.size(); i++){
+			middle=h.top(); h.pop();
+			prev=h.top();
+		while(h.size()>=2 and cross(middle-prev, v[i]-middle)<=0){//while turn right
+			middle=h.top(); h.pop();
+			prev=h.top();
+		}
+		h.push(middle);
+		h.push(v[i]);
+
+	}
+	vector<P> ans(h.size());
+	while(h.size()){
+		ans[h.size()-1]=h.top();
+		h.pop();
+	}
+	return ans;
+}
