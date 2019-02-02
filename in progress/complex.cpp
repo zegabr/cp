5c1371946b43c0d39c5ceba7e013255fd22493a4
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-02-02 13:32:20 -0300

    complex updates

--- in progress/complex.cpp
+++ in progress/complex.cpp
@@ -10,7 +10,7 @@ typedef complex<ld> P;
 #define y imag()
 #define PI 3.1415926535897932384626
 //abs(P) = size
-//norm(P) = size^2
+//norm(P) = size^2 //using norm may lead to rounding errors, use dot(z,z) instead
 //arg(P) = angle in radians
 double angleDegrees(const P &p){
 	return arg(p)*(180/PI);
@@ -35,7 +35,14 @@ ld gcd(ld a, ld b){//gcd between doubles(maybe angles)
 	}
 	return a+b;
 }
+P reflect(const P &p, const P &q1, const P &q2){//reflects p about segment q1->q2
+	P z = p - q1;
+	P w = q2-q1;
+	return conj(z/w)*w + q1;
+}
+P intersect(const P &a,const P &b,const P &p,const P &q){
+	ld d1 = cross(p-a,b-a);
+	ld d2 = cross(q-a,b-a);
+	return (d1*q - d2*p)/(d1-d2);
 
-int main(){
-	
 }
