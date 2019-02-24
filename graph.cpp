f6f1bf739db00be4f903a45de1e3d59a0081c20a
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-02-24 18:41:55 -0300

    kuhn downgraded

--- graph.cpp
+++ graph.cpp
@@ -142,21 +142,15 @@ bool kuhn(int v){//dfs
     return 0;
 }
 
-bool findmatching(){//O(mn)
-	int ans=0,aux=0;
-	clrmatch();
-	bool foundpath;
-	do{
-		tempo++;//unvisit everybody
-		foundpath=0;
-		for(int i=0;i<n;i++){//dfs em todos os vertices do grupo left (0..n-1)
-			if(matchr[i]<0 and vis[i]<tempo)
-				foundpath|=kuhn(i);
+int findmatching(){//O(mn)
+	int ans=0;
+	clr();
+		for(int i=0;i<n;i++){//vertices da esquerda
+			tempo++;
+			ans+=kuhn(i);
 		}
-	}while(foundpath);
-	for(int i=0;i<n;i++) ans+=(matchr[i]>0);
-
-	return ans==n;//perfect match
+	return ans;//qt arestas usadas no match maximo = minima qntidade de vertices que se retirados separam todo o grafo em 2 
+	//return ans==n;//perfect match
 }
 
 void printmatch(){
