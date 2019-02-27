f19e16e34be570fc41175ea02a151a1a48b3d51b
Jose Gabriel Silva Pereira <jgsp2@ug2c22.windows.cin.ufpe.br>
2019-02-27 16:29:11 -0300

    not enough

--- in progress/x.cpp
+++ in progress/x.cpp
@@ -24,23 +24,6 @@ bool isprime(ll n){//O(sqrt(n))
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 
-//debug
-/*
-	ll arr[]={2, 4 ,10 ,16 ,40, 100, 250 ,256 ,400 ,640 ,1600, 16000, 25600};
-	ll to=1;
-	for(int v : arr) to*=v;
-	cout<<to<<pl;
-*/
-	ll teste = 14195346025471803392;
-	ll opa = 160000;
-	ll bug = teste*opa;
-	cout<<bug<<pl;
-	return 0;
-
-//debug
-
-
-
 	ll k;
 	cin>>k;//phi(n)
 	if(k==1){
@@ -50,55 +33,55 @@ int main(){
 		cout<<0<<pl;
 		return 0;
 	}
-	vector<ll> primes;//primes of k
+	set<ll> primesaux;//primes p such p-1 | k
 	ll d;
 	for(ll i=1 ;i*i<=k; i++){
 		if(k%i==0LL){
-			if(isprime(i+1))primes.push_back(i+1);
+			if(isprime(i+1))primesaux.insert(i+1);
 			d=k/i;
-			if(d!=i and isprime(d+1) )primes.push_back(d+1);
+			if(d!=i and isprime(d+1) )primesaux.insert(d+1);
 		}
 	}
+	vector<ll>primes;
+	for(ll a : primesaux)primes.push_back(a);
 	ll tam=primes.size();
-	//cout<<tam<<pl;
-	sort(primes.begin(),primes.end());
+	cout<<tam<<pl;
 	ll pnum,pden;
 	ll ans = inf;
 	ll mask;
+	ll aux;
+	bool ok;
 	for(mask = 1; mask<(1<<tam); mask++){
-		pnum=1ull; pden=1ull;
+		pnum=1ll;
+		ok=1;
+		aux=k;
 		set<ll> used;
-		for(ll i=0;i<tam;i++){
+		for(ll i=0;i<tam and ok;i++){
 			if(mask&(1<<i)){
-				//				cout<<primes[i]<<sp;
-				used.insert(primes[i]);
+				used.insert(primes[i]);//fatores de n
 				pnum*=primes[i];
-				pden*=primes[i]-1;
-				//cout<<pden<<pl;
-								if(pden==0ULL){//degub
-									cout<<i<<pl;
-									cout<<pden<<pl;
-									cout<<primes[i]<<pl;
-									cout<<primes[i]-1<<pl;
-									for(ll f : used)cout<<f-1<<sp;
-									cout<<pl;
-									return 0;
-								}
+				if(aux%(primes[i]-1)==0)aux/=(primes[i]-1);
+				else ok=0;
+
 			}
 		}
-		//cout<<" ==> ";
-		pnum*=k;	
-		if(pnum%pden==0ULL){
-			ll n = pnum/pden;
-			ll aux =n;
-			for(ll p:used){
-				while(aux%p==0LL) aux/=p;
-			}
-			if(aux==1){
-				//cout<<n<<" is a solution"<<pl;
-				ans=min(ans,n);
+		ll aux2=aux;
+		for(ll i=2 ;i*i<=aux2 and ok; i++){
+			if(k%i==0LL){
+				if(used.count(i)==0)ok=0;
+				while(aux2%i==0LL and ok)aux2/=i;
 			}
 		}
+		if(aux2>1 and used.count(aux2)==0)ok=0;//aux2 é primo
+
+		if(ok){
+			ll n = aux*pnum;
+
+			//cout<<n<<" is a solution"<<pl;
+			ans=min(ans,n);
+		}
+
+
 	}
 	//testar para todos os subsets de primes se (k*prodP%prodPminus)==0 
 	cout<<(ans==inf ? 0 : ans)<<pl;	
