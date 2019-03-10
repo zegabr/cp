c2858dbd05429020cb3c43389a7157d28e852bf8
zegabr <jgsp2@gmail.cin.ufpe.br>
2019-03-10 10:00:06 -0300

    useless

---
+++ probablyuseless.cpp
@@ -0,0 +1,31 @@
+//=======matrizes======
+//Transposta(A+B) = Transposta(A) + Transposta(B)
+//Transposta(A*B) = Transposta(B) * Transposta(A)
+//inversa(A*B) = inversa(B) * inversa(A)
+//(AB)C = A(BC)
+//A(B+C) = AB + AC
+//(B+C)A = BA + CA
+//Transposta(inversa(A)) = inversa(Transposta(A))
+//Det(A) = Det(Transposta(A))
+//Det(inversa(A)) = 1/Det(A)
+//Det(k*A) = k^n * Det(A)
+//Det(A*B) = Det(A) * Det(B)
+
+//========geometria=======
+//Area do triangulo de lados a,b e c inscrito na circunferencia de raio R: S = abc/(4R)
+//Area do triangulo circunscrito na circunferencia de raio r: S = r * P = sqrt(P(P-a)(P-b)(P-c)) com P = (a+b+c)/2
+//Poliedros côncavos: #vertices - #arestas + #faces = 2 - 2*(#"buracos")
+//	Fn = #faces de n lados
+//	F = somatório(Fn, n=3 -> n=infinito)
+//	2A = somatorio(n*Fn, n=3 -> n=infinito)
+//	Soma dos angulos internos = 360(V-2) = 360(A+F)
+//
+//Poligonos:
+//	Soma angulos internos = 180(n-2)
+//	#diagonais = n(n-3)/2
+//
+//Espacial:
+//	volume tronco de piramide = h/3 * (A + sqrt(A*a) + a) com a=area da base menor, A=area da base maior e h=distancia entre as bases
+//	volume tronco de cone = PI*n/3 * (R*R + R*r + r*r)
+//	superficie lateral do tronco de cone = PI*g * (R+r) com g sendo a distancia entre as bases na superficie externa
+//	
