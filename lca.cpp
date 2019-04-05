87a5e6cfd11b4642ef9c6b8310e642026382bdff
Jose Gabriel Silva Pereira <jgsp2@ug2c07.windows.cin.ufpe.br>
2019-04-05 20:10:56 -0300

    lca

---
+++ lca.cpp
@@ -0,0 +1,179 @@
+//cp-algorithms lca
+struct LCA {//O(N) preprocessing + O(logN)/query
+    vector<int> height, euler, first, segtree;
+    vector<bool> visited;
+    int n;
+
+    LCA(vector<vector<int>> &adj, int root = 0) {
+        n = adj.size();
+        height.resize(n);
+        first.resize(n);
+        euler.reserve(n * 2);
+        visited.assign(n, false);
+        dfs(adj, root);
+        int m = euler.size();
+        segtree.resize(m * 4);
+        build(1, 0, m - 1);
+    }
+
+    void dfs(vector<vector<int>> &adj, int node, int h = 0) {
+        visited[node] = true;
+        height[node] = h;
+        first[node] = euler.size();
+        euler.push_back(node);
+        for (auto to : adj[node]) {
+            if (!visited[to]) {
+                dfs(adj, to, h + 1);
+                euler.push_back(node);
+            }
+        }
+    }
+
+    void build(int node, int b, int e) {
+        if (b == e) {
+            segtree[node] = euler[b];
+        } else {
+            int mid = (b + e) / 2;
+            build(node << 1, b, mid);
+            build(node << 1 | 1, mid + 1, e);
+            int l = segtree[node << 1], r = segtree[node << 1 | 1];
+            segtree[node] = (height[l] < height[r]) ? l : r;
+        }
+    }
+
+    int query(int node, int b, int e, int L, int R) {
+        if (b > R || e < L)
+            return -1;
+        if (b >= L && e <= R)
+            return segtree[node];
+        int mid = (b + e) >> 1;
+
+        int left = query(node << 1, b, mid, L, R);
+        int right = query(node << 1 | 1, mid + 1, e, L, R);
+        if (left == -1) return right;
+        if (right == -1) return left;
+        return height[left] < height[right] ? left : right;
+    }
+
+    int lca(int u, int v) {
+        int left = first[u], right = first[v];
+        if (left > right)
+            swap(left, right);
+        return query(1, 0, euler.size() - 1, left, right);
+    }
+};
+//=============================================================
+//O(NlogN) preprocessing + O(log N)/query
+int n, l;
+vector<vector<int>> adj;
+
+int timer;
+vector<int> tin, tout;
+vector<vector<int>> up;
+
+void dfs(int v, int p)
+{
+    tin[v] = ++timer;
+    up[v][0] = p;
+    for (int i = 1; i <= l; ++i)
+        up[v][i] = up[up[v][i-1]][i-1];
+
+    for (int u : adj[v]) {
+        if (u != p)
+            dfs(u, v);
+    }
+
+    tout[v] = ++timer;
+}
+
+bool is_ancestor(int u, int v)
+{
+    return tin[u] <= tin[v] && tout[u] >= tout[v];
+}
+
+int lca(int u, int v)
+{
+    if (is_ancestor(u, v))
+        return u;
+    if (is_ancestor(v, u))
+        return v;
+    for (int i = l; i >= 0; --i) {
+        if (!is_ancestor(up[u][i], v))
+            u = up[u][i];
+    }
+    return up[u][0];
+}
+
+void preprocess(int root) {
+    tin.resize(n);
+    tout.resize(n);
+    timer = 0;
+    l = ceil(log2(n));
+    up.assign(n, vector<int>(l + 1));
+    dfs(root, root);
+}
+
+//============================================================
+//LCA for offline queries = O(m+n) preprocessing + O(1)/query
+vector<vector<int>> adj;
+vector<vector<int>> queries;
+vector<int> ancestor;
+vector<bool> visited;
+
+void dfs(int v)
+{
+    visited[v] = true;
+    ancestor[v] = v;
+    for (int u : adj[v]) {
+        if (!visited[u]) {
+            dfs(u);
+            union_sets(v, u);
+            ancestor[find_set(v)] = v;
+        }
+    }
+    for (int other_node : queries[v]) {
+        if (visited[other_node])
+            cout << "LCA of " << v << " and " << other_node
+                 << " is " << ancestor[find_set(other_node)] << ".\n";
+    }
+}
+
+void compute_LCAs() {
+    // initialize n, adj and DSU
+    // for (each query (u, v)) {
+    //    queries[u].push_back(v);
+    //    queries[v].push_back(u);
+    // }
+
+    ancestor.resize(n);
+    visited.assign(n, false);
+    dfs(0);
+}
+
+//=============================================
+//gap implementation
+int par[ms][mlg+1], lvl[ms];
+vector<int> g[ms];
+
+void dfs(int v, int p, int l = 0) { // chamar como dfs(root, root)
+  lvl[v] = l;
+  par[v][0] = p;
+  for(int k = 1; k <= mlg; k++) {
+    par[v][k] = par[par[v][k-1]][k-1];
+  }
+  for(int u : g[v]) {
+    if(u != p) dfs(u, v, l + 1);
+  }
+}
+
+int lca(int a, int b) {
+  if(lvl[b] > lvl[a]) swap(a, b);
+  for(int i = mlg; i >= 0; i--) {
+    if(lvl[a] - (1 << i) >= lvl[b]) a = par[a][i];
+  }
+  if(a == b) return a;
+  for(int i = mlg; i >= 0; i--) {
+    if(par[a][i] != par[b][i]) a = par[a][i], b = par[b][i];
+  }
+  return par[a][0];
+}
