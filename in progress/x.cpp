b612ee587b8449ff821114ee720fd7008c9d7822
Jose Gabriel Silva Pereira <jgsp2@ug2c06.windows.cin.ufpe.br>
2019-04-09 16:10:08 -0300

    make 2 to test

--- in progress/x.cpp
+++ in progress/x.cpp
@@ -9,9 +9,9 @@ int pd[10001][910][3];//index, current capacity, how many can activate
 int p[10001],w[10001],d[10001];
 int m,n;
 
-//current capacity
+//offset capacity
 int S;
-inline int cc(int j){ return j+200;}
+inline int off(int j){ return j+S;}
 
 
 int main(){
@@ -25,38 +25,37 @@ int main(){
 		if(d[i]>me2)me2=d[i];
 		if(me2>me)swap(me,me2);
 	}
-	
+
 	S=me+me2;
-	
+
 	for(int i=1;i<=n;i++){
-		for(int j=cc(0);j<=cc(m);j++){
+		for(int j=off(0);j<=off(m);j++){
 			for(int a=0;a<3;a++){
 				//caso de pegar ou nao pegar sem ativar
-				if(cc(w[i])>j)pd[i][j][a] = pd[i-1][j][a];//n pode pegar
+				if(off(w[i])>j)pd[i][j][a] = pd[i-1][j][a];//n pode pegar
 				else pd[i][j][a] = max(pd[i-1][j][a], pd[i-1][j-w[i]][a]+p[i]);//nao pega ou pega 
-
-				if(a){//pode pegar ativando
-					if(j+d[i]-w[i]>=200)	pd[i][j+d[i]][a-1] = max(pd[i-1][j+d[i]][a-1], pd[i-1][j+d[i]-w[i]][a]+p[i]);//nao pegar ou pegar (ativando)
-				} 
+	
+				if(a){	
+					pd[i][j+d[i]][a-1] = max(pd[i-1][j+d[i]][a-1], pd[i-1][j+d[i]-w[i]][a]+p[i]);//nao pegar ou pegar (ativando)
+				}
 
 			}
+
 		}
 	}
 
 	int ans=0;
-//	cout<<S<<pl;
+	//	cout<<S<<pl;
 	for(int i=1;i<=n;i++)
-		for(int j=cc(1);j<=cc(m);j++)
-			for(int a=0;a<3;a++){
+		for(int j=off(1);j<=off(m);j++)
+			for(int a=0;a<3;a++)
 				ans = max(ans, pd[i][j][a]);
-//				if(ans == 300) cout<<i<<sp<<j<<sp<<a<<pl;
-			}
-	cout<<pd[n][cc(m)][0]<<pl;
-	cout<<pd[n][cc(m)][1]<<pl;
-	cout<<pd[n][cc(m)][2]<<pl;
+
+
+//	cout<<pd[n][off(m)][0]<<pl;
 	cout<<ans<<pl;
 
 
 
-	
+
 }
