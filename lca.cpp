7f2c70e48fbb0ed6a3e2a448dd56176bd9a16bea
Jose Gabriel Silva Pereira <jgsp2@ug3c16.windows.cin.ufpe.br>
2019-04-12 19:45:16 -0300

    lca

--- lca.cpp
+++ lca.cpp
@@ -1,77 +1,28 @@
-//cp-algorithms lca
-//O(NlogN) preprocessing + O(log N)/query
-int n, l;
-vector<vector<int>> adj;
+//====lca==============
+const int N=1e4+10, ln=25;
+int up[N][ln], lv[N];
+vector<int> g[N];
 
-int timer;
-vector<int> tin, tout;
-vector<vector<int>> up;
-
-void dfs(int v, int p)
-{
-    tin[v] = ++timer;
-    up[v][0] = p;
-    for (int i = 1; i <= l; ++i)
-        up[v][i] = up[up[v][i-1]][i-1];
-
-    for (int u : adj[v]) {
-        if (u != p)
-            dfs(u, v);
-    }
-
-    tout[v] = ++timer;
+void dfs(int u,int p, int h=0){
+	lv[u]=h;
+	up[u][0] = p;
+	for(int i=1; i<ln;i++) up[u][i] = up[up[u][i-1]][i-1];
+	for(int v : g[u]){
+		if(v!=p) dfs(v, u, h+1);
+	}
 }
 
-bool is_ancestor(int u, int v)
-{
-    return tin[u] <= tin[v] && tout[u] >= tout[v];
+int lca(int a, int b){
+	if(lv[b]>lv[a]) swap(a,b);
+	int d = lv[a]-lv[b];
+	for(int i=0;i<ln;i++) if((d>>i)&1) a = up[a][i];
+	if(a==b) return a;
+	for(int i=ln; i>-1 ; i--){
+		if(up[a][i] != up[b][i]){
+			a=up[a][i];
+			b=up[b][i];
+		}
+	}
+	return up[a][0]; 
 }
 
-int lca(int u, int v)
-{
-    if (is_ancestor(u, v))
-        return u;
-    if (is_ancestor(v, u))
-        return v;
-    for (int i = l; i >= 0; --i) {
-        if (!is_ancestor(up[u][i], v))
-            u = up[u][i];
-    }
-    return up[u][0];
-}
-
-void preprocess(int root) {
-    tin.resize(n);
-    tout.resize(n);
-    timer = 0;
-    l = ceil(log2(n));
-    up.assign(n, vector<int>(l + 1));
-    dfs(root, root);
-}
-//==================================================
-//gap implementation
-int par[ms][mlg+1], lvl[ms];
-vector<int> g[ms];
-
-void dfs(int v, int p, int l = 0) { // chamar como dfs(root, root)
-  lvl[v] = l;
-  par[v][0] = p;
-  for(int k = 1; k <= mlg; k++) {
-    par[v][k] = par[par[v][k-1]][k-1];
-  }
-  for(int u : g[v]) {
-    if(u != p) dfs(u, v, l + 1);
-  }
-}
-
-int lca(int a, int b) {
-  if(lvl[b] > lvl[a]) swap(a, b);
-  for(int i = mlg; i >= 0; i--) {
-    if(lvl[a] - (1 << i) >= lvl[b]) a = par[a][i];
-  }
-  if(a == b) return a;
-  for(int i = mlg; i >= 0; i--) {
-    if(par[a][i] != par[b][i]) a = par[a][i], b = par[b][i];
-  }
-  return par[a][0];
-}
