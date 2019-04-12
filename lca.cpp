a64cc7aa43639ec7692af135b53d2ea5a1a39d11
Jose Gabriel Silva Pereira <jgsp2@ug3c16.windows.cin.ufpe.br>
2019-04-12 20:08:35 -0300

    lca update

--- lca.cpp
+++ lca.cpp
@@ -1,12 +1,12 @@
 //====lca==============
 const int N=1e4+10, ln=25;
-int up[N][ln], lv[N];
+int up[N][ln+1], lv[N];
 vector<int> g[N];
 
 void dfs(int u,int p, int h=0){
 	lv[u]=h;
 	up[u][0] = p;
-	for(int i=1; i<ln;i++) up[u][i] = up[up[u][i-1]][i-1];
+	for(int i=1; i<=ln;i++) up[u][i] = up[up[u][i-1]][i-1];
 	for(int v : g[u]){
 		if(v!=p) dfs(v, u, h+1);
 	}
