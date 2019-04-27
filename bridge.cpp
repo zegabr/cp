993a6269ff01c6b7d33d5178811e9670ee10f16e
Jose Gabriel Silva Pereira <jgsp2@ug3c36.windows.cin.ufpe.br>
2019-04-27 14:42:27 -0300

    bridges

---
+++ bridge.cpp
@@ -0,0 +1,33 @@
+//find bridges in graphs by cp-algorithms
+int n; // number of nodes
+vecvr<vecvr<int>> g; // gacency list of graph
+
+vecvr<bool> vis;
+vecvr<int> tin, low;
+int timer;
+
+void dfs(int u, int p = -1) {
+	vis[u] = 1;
+	tin[u] = low[u] = timer++;
+	for (int v : g[u]) {
+		if (v == p) continue;
+		if (vis[v]) {
+			low[u] = min(low[u], tin[v]);
+		} else {
+			dfs(v, u);
+			low[u] = min(low[u], low[v]);
+			if (low[v] > tin[u])
+				//IS_BRIDGE(u, v);
+		}
+	}
+}
+void find_bridges() {
+	timer = 0;
+	vis.assign(n, 0);
+	tin.assign(n, -1);
+	low.assign(n, -1);
+	for (int i = 0; i < n; ++i) {
+		if (!vis[i])
+			dfs(i);
+	}
+}
