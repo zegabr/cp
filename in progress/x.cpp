a6f7ae1dc3c8795619fab3b6a08642f2ee956846
Jose Gabriel Silva Pereira <jgsp2@ug3c12.windows.cin.ufpe.br>
2019-05-08 09:20:40 -0300

    .

--- in progress/x.cpp
+++ in progress/x.cpp
@@ -7,8 +7,10 @@ typedef long long ll;
 
 int a[100][5][5];
 int n;
+typedef pair<int,int> ii;
+priority_queue<ii,vector<ii>,greater<ii>> pq;
 
-bool eat(int c1, int c2){//empatam antes de todos
+void testa(int c1, int c2){//testa se empatam e se alguma cartela empata antes
 	bool rep=0;
 	for(int i=0;i<5;i++){
 		for(int j=0;j<5;j++){
@@ -24,30 +26,35 @@ bool eat(int c1, int c2){//empatam antes de todos
 					for(int col=0;col<5 and allin;col++){
 						allin &= s.count(a[k][lin][col]);
 					}
-					if(allin)return 0;//outra linha ganha antes de c1 e c2 empatarem
+					if(allin){
+						cout<<"no ties"<<pl;	
+						exit(0);//outra linha ganha antes de c1 e c2 empatarem
+					}
 				}
 			}
 		}
 	}
-	return rep;
+	if(rep) pq.push({c1,c2});
 }
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
 	cin>>n;
 	for(int i=0;i<n;i++)
-	for(int j=0;j<5;j++)
-	for(int k=0;k<5;k++)
-		cin>>a[i][j][k];
+		for(int j=0;j<5;j++)
+			for(int k=0;k<5;k++)
+				cin>>a[i][j][k];
 
 	for(int c1=0;c1<n-1;c1++){
 		for(int c2=c1+1;c2<n;c2++){
-			if(eat(c1,c2)){
-				cout<<c1+1<<sp<<c2+1<<pl;
-				return 0;
-			}
+			testa(c1,c2);
 		}
 	}
 
-	cout<<"no ties\n";
+	if(pq.empty()) cout<<"no ties\n";
+	else cout<<pq.top().first+1<<sp<<pq.top().second+1<<pl;
+
+
+
+
 }
