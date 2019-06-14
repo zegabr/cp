f844c24cfc34edca8a6807c8e885f29e17a1fe58
zegabr <jgsp2@cin.ufpe.br>
2019-06-14 11:12:01 -0300

    grid moving diagonals

--- graph.cpp
+++ graph.cpp
@@ -1,7 +1,7 @@
 //==========GRID MOVING=======
-int movx[] = {-1, 0, 1, 0};
-int movy[] = {0, 1, 0, -1};
-for(int i = 0; i < 4;++i){//up,right,down,left
+int movx[] = {-1, 0, 1, 0,-1, 1, 1, -1};
+int movy[] = {0, 1, 0, -1, 1, 1, -1, -1};
+for(int i = 0; i < 8;++i){//u,r,d,l,ur,dr,dl,ul
 	int nx = movx[i] + x;
 	int ny = movy[i] + y;
 	//if safe then dfs(nx,ny)
