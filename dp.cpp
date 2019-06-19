4109c5c62a30a489049badda6d9a334d5507af07
zegabr <jgsp2@cin.ufpe.br>
2019-06-19 18:50:02 -0300

    nlogn lis

--- dp.cpp
+++ dp.cpp
@@ -1,4 +1,4 @@
-//////MAX SUBARRAY SUM//////
+//==========MAX SUBARRAY SUM============
 //int dp[maxn],a[maxn],ans=-maxn;
 int maxsubarraysum(int n){
 	dp[0]=0;
@@ -10,6 +10,37 @@ int maxsubarraysum(int n){
 	return ans;
 }
 
+
+//==================LIS (nlogn)==========
+int ceilindex(vector<int> &v,vector<int> &T, int l, int r, int key){
+	while(r-l > 1){
+		int m = l + (r-l)/2;
+		if(v[T[m]]>=key) r=m;
+		else l=m;
+	}
+	return r;
+}
+vector<int> LIS(vector<int> &v){
+	vector<int> ans;
+	if(v.size()==0) return ans;
+	vector<int> tail(v.size(),0);
+	vector<int> prev(v.size(),-1);
+	int len = 1;
+	for(int i=1;i<v.size();i++){
+		if(v[i] < v[tail[0]]) tail[0]=i;
+		else if(v[i] > v[tail[len-1]]){
+			prev[i] = tail[len-1];
+			tail[len++] = i;
+		}else{
+			int pos = ceilindex(v,tail, -1, len-1 , v[i]);
+			prev[i] = tail[pos-1];
+			tail[pos]=i;
+		}
+	}
+	for(int i=tail[len-1]; i>=0; i=prev[i])ans.push_back(v[i]);
+	reverse(ans.begin(),ans.end());
+	return ans;
+}
 ///////EGGDROP ITERATIVO//////////////
 
 
