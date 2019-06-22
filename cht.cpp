ae066afcad8d803467e5f95fc47a9eeb71beab7b
zegabr <jgsp2@cin.ufpe.br>
2019-06-21 21:38:57 -0300

    cht update

--- cht.cpp
+++ cht.cpp
@@ -1,115 +1,57 @@
 //CONVEX HULL TRICK
-//cp-algorithms
-typedef int ftype;
-typedef complex<ftype> point;
-#define x real
-#define y imag
-
-ftype dot(point a, point b) {
-    return (conj(a) * b).x();
-}
-
-ftype cross(point a, point b) {
-    return (conj(a) * b).y();
-}
-
-vector<point> hull, vecs;
-
-void add_line(ftype k, ftype b) {
-    point nw = {k, b};
-    while(!vecs.empty() && dot(vecs.back(), nw - hull.back()) < 0) {
-        hull.pop_back();
-        vecs.pop_back();
-    }
-    if(!hull.empty()) {
-        vecs.push_back(1i * (nw - hull.back()));
-    }
-    hull.push_back(nw);
-}
-
-int get(ftype x) {
-    point query = {x, 1};
-    auto it = lower_bound(vecs.begin(), vecs.end(), query, [](point a, point b) {
-        return cross(a, b) > 0;
-    });
-    return dot(query, hull[it - vecs.begin()]);
-}
-
-//================================================================
-//li-chao-tree
-typedef int ftype;
-typedef complex<ftype> point;
-#define x real
-#define y imag
-
-ftype dot(point a, point b) {
-    return (conj(a) * b).x();
-}
-
-ftype f(point a,  ftype x) {
-    return dot(a, {x, 1});
-}
-const int maxn = 2e5;
-point line[4 * maxn];
-
-void add_line(point nw, int v = 1, int l = 0, int r = maxn) {
-    int m = (l + r) / 2;
-    bool lef = f(nw, l) < f(line[v], l);
-    bool mid = f(nw, m) < f(line[v], m);
-    if(mid) {
-        swap(line[v], nw);
-    }
-    if(r - l == 1) {
-        return;
-    } else if(lef != mid) {
-        add_line(nw, 2 * v, l, m);
-    } else {
-        add_line(nw, 2 * v + 1, m, r);
-    }
-}
-int get(int x, int v = 1, int l = 0, int r = maxn) {
-    int m = (l + r) / 2;
-    if(r - l == 1) {
-        return f(line[v], x);
-    } else if(x < m) {
-        return min(f(line[v], x), get(x, 2 * v, l, m));
-    } else {
-        return min(f(line[v], x), get(x, 2 * v + 1, m, r));
-    }
-}
-//======================https://github.com/niklasb/contest-algos/blob/master/convex_hull/dynamic.cpp
-const ll is_query = -(1LL<<62);
-struct Line {
-    ll m, b;
-    mutable function<const Line*()> succ;
-    bool operator<(const Line& rhs) const {
-        if (rhs.b != is_query) return m < rhs.m;
-        const Line* s = succ();
-        if (!s) return 0;
-        ll x = rhs.m;
-        return b - s->b < (s->m - m) * x;
-    }
+//by kkinhoz eu acho
+struct line{
+	ll m,c;
 };
-struct HullDynamic : public multiset<Line> { // will maintain upper hull for maximum
-    bool bad(iterator y) {
-        auto z = next(y);
-        if (y == begin()) {
-            if (z == end()) return 0;
-            return y->m == z->m && y->b <= z->b;
-        }
-        auto x = prev(y);
-        if (z == end()) return y->m == x->m && y->b <= x->b;
-        return (x->b - y->b)*(z->m - y->m) >= (y->b - z->b)*(y->m - x->m);
-    }
-    void insert_line(ll m, ll b) {
-        auto y = insert({ m, b });
-        y->succ = [=] { return next(y) == end() ? 0 : &*next(y); };
-        if (bad(y)) { erase(y); return; }
-        while (next(y) != end() && bad(next(y))) erase(next(y));
-        while (y != begin() && bad(prev(y))) erase(prev(y));
-    }
-    ll eval(ll x) {
-        auto l = *lower_bound((Line) { x, is_query });
-        return l.m * x + l.b;
-    }
-};
+
+deque<line> hull;
+
+inline ld inter(line &t1,line &t2){
+	return (ld)(t2.c - t1.c)/(t1.m - t2.m);
+}
+
+void add(ll m, ll c){
+
+	line novo,L1,L2;
+	novo.m=m;
+	novo.c=c;
+	ld p1,p2;
+	bool ok = true;
+
+	while(hull.size() > 1 and ok){
+		L1 = hull.back();
+		hull.pop_back();
+		L2 = hull.back();
+
+		p1 = inter(L1,L2);
+		p2 = inter(L1,novo);
+
+		if(p2 < p1){
+			hull.push_back(L1);
+			ok = false;
+		}
+	}
+	hull.push_back(novo);
+}
+
+ll get(ll &x){
+	if(hull.size() == 0) return 0;
+
+	int l = 0;
+	int r = hull.size() - 1;
+	int m = (l+r)/2;
+	ld p;
+
+	while(l < r){
+		p = inter(hull[m],hull[m+1]);
+		if(p < x){
+			r = m;
+			m = (l+r)/2;
+		}
+		else{
+			l = m+1;
+			m = (l+r)/2;
+		}
+	}
+	return (hull[m].m * x) + hull[m].c;
+}
