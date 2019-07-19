fabeeb51a84e1c3bafbba434ee78fd3ed4afc2dd
zegabr <jgsp2@cin.ufpe.br>
2019-07-18 22:29:18 -0300

    2 in 1 reusable
    cut vertex/ bridges in one code

--- graph/bridge-pointt-articulation.cpp
+++ graph/bridge-pointt-articulation.cpp
@@ -1,37 +1,45 @@
 //====================================================
 const int unvisited = 0, visited=1, explored=2,V=1e6+5;
 int timer, dfsroot, rootchildren;
-vector<int>num(V), low(V), par(V), cutvertex(V), g[V];
+vector<int>num, low, par, cutvertex, g[V];
 
-void artpoints(int u){
+inline void iscutvertex(int v){
+
+}
+
+inline void isbridge(int u, int v){
+
+}
+
+void dfspointbridge(int u){
 	low[u]=num[u]=timer++;
 	for(int v : g[u]){
 		if(num[v]==unvisited){
 			par[v]=u;
 			if(u == dfsroot) rootchildren++;
 
-			artpoints(v);
+			dfspointbridge(v);
 
 			if(low[v]>=num[u]){
-				//cutvertex[u]=1;//vertex u is articulation point
+				iscutvertex(v);
 			}
 			if(low[v]>num[u]){
-				//edge u v is bridge
+				isbridge(u,v);
 			}
 			low[u]=min(low[u], low[v]);
 		}else if(v != par[u]) low[u]=min(low[u],num[v]);
 	}
 }
 
-void find(){
+void findpointbridge(int V){
 	timer=0;
-	num.assign(V,unvisited);
-	low.assign(V,0);
-	par.assign(V,0);
-	cutvertex.assign(V,0);
+	num.assign(V+1,unvisited);
+	low.assign(V+1,0);
+	par.assign(V+1,0);
+	cutvertex.assign(V+1,0);
 	for(int i=0;i<V;i++){
 		if(num[i]==unvisited){
-			dfsroot=i; rootchildren=0; artpoints(i);
+			dfsroot=i; rootchildren=0; dfspointbridge(i);
 			cutvertex[i] = rootchildren > 1;
 		}
 	}
