318187b5d84be8d48edec0ae77fccbdb1a0a5471
zegabr <jgsp2@cin.ufpe.br>
2019-07-18 22:38:22 -0300

    added bellman-ford
    negative wheighted SSP

---
+++ graph/bellman-ford.cpp
@@ -0,0 +1,10 @@
+//========================
+vector<int> dist(V,inf); dist[s]=0; 
+for(int i=0 ; i<V-1 ; i++){
+    for(int u=0 ; u<V ; u++){
+        for(auto &v :    g[u]){
+            dist[v.first] = min(dist[v.first], dist[u]+v.second);
+        }
+    }
+}
+//=========================
