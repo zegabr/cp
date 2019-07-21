c4aaaab49654f784f7ae228d1af6c821bdb62c0f
zegabr <jgsp2@cin.ufpe.br>
2019-07-20 22:01:07 -0300

    python cheat sheet

---
+++ pythoncheatsheet.py
@@ -0,0 +1,14 @@
+'''
+allinput = stdin.read() #read all input as string (till EOF)
+line = stdin.readline() #read one line as string (do not cause errors if eof)
+a1,a2,...,an = map(int, stdin.readline().split()) #get n ints
+char = stdin.read(1) #read one char
+file = open("arq.in",'r') #now file has read() and readline()
+a,b,c = sorted([int(x) for x in input().strip().split()]) #multiple init
+print(a,b,c)
+'''
+'''builtin usefull functions: pow,sum,len,abs,max,min,sorted,reversed,bin,chr,ord,hex,oct,list,set,dict'''
+'''set() interface: add, discard, clear, in, not in, |, &, ^,update(iteralbe)'''
+'''dict() interface: d[key]=value, key in d, key not in d, clear, update(iteralbe)'''
+'''
+'''
