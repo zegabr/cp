529922aac19f43cde7b398814531a6f1cbf1d44e
zegabr <jgsp2@cin.ufpe.br>
2019-07-22 18:07:28 -0300

    extended euclides fully working
    just call positiveSolution

--- numtheory/numtheory.cpp
+++ numtheory/numtheory.cpp
@@ -82,7 +82,7 @@ ll countdiv(ll n){//O(sqrt(n))
 	return c;
 }
 
-//=========EUCLIDES ESTENDIDmO==============
+//=========EUCLIDES ESTENDIDO==============
 // acha x e y da equacao:
 // a * x + b * y = gcd(a, b);
 // x eh inverso modular de a no modulo y
@@ -95,6 +95,28 @@ void euclides(ll a, ll b, ll &x, ll &y, ll &d) {
 	int y1=x-(a/b)*y;
 	x=x1; y=y1;
 
+}
+
+void makePositive(ll &x,ll &y,ll a, ll b){
+	if(x<0){
+		ll g = gcd(a,b);
+		ll n = (ll)ceil((double)-x*g/b);
+		x = x+n*b/g;
+		y = y-n*a/g;
+	}else if(y<0){
+		ll g = gcd(a,b);
+		ll n = (ll)ceil((double)-y*g/a);
+		x = x-n*b/g;
+		y = y+n*a/g;
+	}
+}
+//ax+by=k
+void positiveSolution(ll a, ll b, ll k, ll &x, ll &y, ll &d){
+	euclides(a,b,x,y,d);
+	ll g=gcd(a,b);
+	x=k*x/g;
+	y=k*y/g;
+	makePositive(x,y,a,b);
 }
 
 //===============FAST EXPONENTIATION=================
