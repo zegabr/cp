f40cb3224a785cf76b64260edc9000a56ed81401
zegabr <jgsp2@cin.ufpe.br>
2019-07-22 13:05:06 -0300

    math functions

--- pythoncheatsheet.py
+++ pythoncheatsheet.py
@@ -7,6 +7,7 @@ file = open("arq.in",'r') #now file has read() and readline()
 a,b,c = sorted([int(x) for x in input().strip().split()]) #multiple init
 print(a,b,c)
 
+math functions: ceil,floor,fabs,factorial,fsum,gcd,log,sqrt,hypot,acos,asin,atan,cos,sin,tan,degrees,radians,pi,pow as fpow
 builtin usefull functions: pow,sum,len,abs,max,min,sorted,reversed,bin,chr,ord,hex,oct,list,set,dict
 set() interface: add, discard, clear, in, not in, |, &, ^,update(iteralbe)
 dict() interface: d[key]=value, key in d, key not in d, clear, update(iteralbe)
