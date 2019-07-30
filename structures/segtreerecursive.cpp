50da33cbd53fc1ad55d1392001324e306356070e
zegabr <jgsp2@cin.ufpe.br>
2019-07-30 18:09:17 -0300

    segtree easier to change, but not overpowewr yet

--- structures/segtreerecursive.cpp
+++ structures/segtreerecursive.cpp
@@ -1,20 +1,9 @@
 ///=====================================================
-int arr[4 * N], seg[4 * N], n;
-void build(int x = 1, int l = 0, int r = n - 1) {
-	if(l == r) {
-		seg[x] = arr[l];
-		return;
-	}
-	int m = (l+r)>>1,e=x+x, d=e+1;
-	build(e, l, m); build(d, m + 1, r);
-	seg[x] = seg[e] + seg[d]; // Merge
-}
+int a[4 * N], seg[4 * N], n;
+const int none = 0x3f3f3f3f;//padrao retornado pela query tal que nao interfira
 
-int query(int a, int b, int x = 1, int l = 0, int r = n - 1) {
-	if(b < l || a > r) return 0; // Valor que nao atrapalhe
-	if(a <= l && r <= b) return seg[x];
-	int m = (l+r)>>1, e=x+x, d=e+1; 
-	return query(a, b, e, l, m) + query(a, b, d, m + 1, r); // Merge
+inline int merge(int e, int d){
+	return min(e,d);
 }
 
 void update(int a, int val, int x = 1, int l = 0, int r = n -1) {
@@ -25,6 +14,23 @@ void update(int a, int val, int x = 1, int l = 0, int r = n -1) {
 	}
 	int m = (l+r)/2, e = x+x, d = e+1;
 	update(a,val, e, l, m); update(a, val, d, m + 1, r);
-	seg[x] = seg[e] + seg[d]; // Merge
+	seg[x] = merge(seg[e] , seg[d]); 
+}
+
+int query(int a, int b, int x = 1, int l = 0, int r = n - 1) {
+	if(b < l || a > r) return none; // Valor que nao atrapalhe
+	if(a <= l && r <= b) return seg[x];
+	int m = (l+r)>>1, e=x+x, d=e+1; 
+	return merge(query(a, b, e, l, m), query(a, b, d, m + 1, r)); 
+}
+
+void build(int x = 1, int l = 0, int r = n - 1) {
+	if(l == r) {
+		seg[x] = a[l];
+		return;
+	}
+	int m = (l+r)>>1,e=x+x, d=e+1;
+	build(e, l, m); build(d, m + 1, r);
+	seg[x] = merge(seg[e], seg[d]);
 }
 //====================================================
