c1a4e271f3724f4fcda8892603d675282fe2936f
José Gabriel <jgsp2@cin.ufpe.br>
2019-09-10 21:05:54 -0300

    Update grid.cpp

--- graph/grid.cpp
+++ graph/grid.cpp
@@ -3,9 +3,11 @@
 //j grows from left->right
 int movi[] = {-1, 0, 1, 0,-1, 1, 1, -1};
 int movj[] = {0, 1, 0, -1, 1, 1, -1, -1};
+/*
 for(int i = 0; i < 8;++i){//u,r,d,l,ur,dr,dl,ul
 	int nx = movi[i] + x;
 	int ny = movj[i] + y;
 	//if safe then dfs(nx,ny)
 }
+*/
 //====================================================
