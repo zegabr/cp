4b83fce3210f239a21d4fa1b8aaebd1d2a37cb65
Jose Gabriel Silva Pereira <jgsp2@ug2c32.windows.cin.ufpe.br>
2019-09-24 10:34:04 -0300

    toposort update

--- graph/toposort.cpp
+++ graph/toposort.cpp
@@ -1,15 +1,15 @@
-vector<int> g[ms], vis(ms);
-map<int,int>grau;
+vector<int> g[ms];
+map<int,int>grau;//de entrada
+vector<int> ans;
+
 void toposort(){//O(V+E)
 	priority_queue<int, vector<int>,greater<int>> pq;
-	for(int i=0;i<n;i++)if(grau[i]==0)pq.push(i);
+	for(int i=0;i<n;i++)if(!grau[i])pq.push(i);
 	while(pq.size()){
 		int u = pq.top(); pq.pop();
 		for(auto v : g[u]){
-			grau[v]--;
-			if(!vis[v] and grau[v]==0) pq.push(v);
+			if(--grau[v]==0) pq.push(v);
 		}
-		topo.push_back(u);
-		vis[u]=1;
+		ans.push_back(u);
 	}
 }
