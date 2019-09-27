f96658eb6ac3ac1e33d4bc11170e2d81d65a1e0c
zegabr <jgsp2@cin.ufpe.br>
2019-09-26 21:45:12 -0300

    suffix array string matching mlogn

--- string/suffixarray.cpp
+++ string/suffixarray.cpp
@@ -1,11 +1,13 @@
 #include <cstring>
 const int ms = 111111;
-string T;
+char T[ms];
+char P[ms];
 int ra[ms], tra[ms];
 int sa[ms], tsa[ms];
 int c[ms];
+
 void countingSort(int k){
-	int n = T.size();
+	int n = (int)strlen(T);
 	int i, sum, maxi = max(300, n);
 	memset(c, 0 , sizeof c);
 	for(i=0;i<n;i++)
@@ -23,8 +25,8 @@ void countingSort(int k){
 
 void buildSA(){
 	int i, k, r;
-	T+='$';
-	int n = T.size();
+	int n = (int)strlen(T);
+	T[n++]='$';
 	for(i=0;i<n;i++) ra[i]=T[i];
 	for(i=0;i<n;i++) sa[i]=i;
 	for(k=1;k<n;k<<=1){
@@ -37,9 +39,37 @@ void buildSA(){
 			ra[i]=tra[i];
 		if(ra[sa[n-1]]==n-1) break;
 	}
-/*	
-	for(int i=0;i<n;i++)
-		cout << sa[i] << "  " << T.substr(sa[i]) << endl;
-*/
+	
+	for(int i=0;i<n;i++) printf("%2d\t%s\n",sa[i], T+sa[i]);
+		cout << sa[i] << "  " << T+sa[i] << endl;
+
 }
 
+pair<int,int> stringMatching(char P[]){
+	int n = (int)strlen(T);//because when it is called, $ is at end of T
+	int m = (int)strlen(P);
+	int lo=0, hi=n-1, mid = lo;
+	while(lo < hi){
+		mid = (lo+hi)/2;
+		int res = strncmp(T+sa[mid],P,m);
+		if(res>=0)hi=mid;
+		else lo = mid+1;
+	}
+	if(strncmp(T+sa[lo], P, m)!=0) return ii(-1,-1);//fail
+	ii ans; ans.first = lo;
+	lo = 0; hi = n-1; mid = lo;
+	while(lo < hi){
+		mid = (lo+hi)/2;
+		int res = strncmp(T+sa[mid],P,m);
+		if(res>0)hi=mid;
+		else lo = mid+1;
+	}
+	if(strncmp(T+sa[lo], P, m)!=0) hi--;
+	ans.second = hi;
+	return ans;
+}
+
+void stringCopy(char t[], string &s){
+	for(int i=0;i<s.size();i++)
+		t[i]=s[i];
+}
