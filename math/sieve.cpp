ef2c8f72b09e3883ed4e47cea5a20a504f36b319
zegabr <jgsp2@cin.ufpe.br>
2019-10-17 22:01:57 -0300

    sieve update

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -9,14 +9,17 @@ void sieve (){
 		for(ll i=p*p; i<=P; i+=p)
 			prime[i]=0; 
 	}
-	for(int i=2;i<=P;i++)
+	primes.pb(2);
+	for(int i=3;i<=P;i+=2)
 		if(prime[i])
 			primes.push_back(i);
 }
 
 bool isPrime(ll n){
 	if(n<prime.size()) return prime[n];
-	for(int i=0;i<primes.size();i++) if(n%primes[i]==0) return 0;
+	for(int i=0;i<primes.size() and primes[i]*primes[i]<=n;i++) 
+		if(n%primes[i]==0) 
+			return 0;
 	return 1;
 }
 
