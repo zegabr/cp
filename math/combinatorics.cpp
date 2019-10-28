0af9013fc6de365a8b55b58aea88f6bf55f8d932
zegabr <jgsp2@cin.ufpe.br>
2019-10-28 14:26:31 -0300

    derrangements

--- math/combinatorics.cpp
+++ math/combinatorics.cpp
@@ -1,4 +1,34 @@
 
+
+//========CATALAN NUMBERS=========
+/*
+	 -numero de Dyck words de tamanho 2n (strings com n X's e n Y's tal que nenhum prefixo tem mais X's do que Y's)
+	 -fazendo X==>'(' e Y==>')', catalan(n) é o numero de expressoes de parenteses válidas (com n pares de parenteses)
+	 -numero de maneiras de aplicar n operacoes binarias em n+1 fatores utilizando associatividade (numero de maneiras de parentizar n+1 fatores)
+	 -numero de arvores binarias com n+1 folhas
+	 -numero de caminhos monotonicos numa grid nxn
+	 -numero de maneiras de triangular polígono convexo de n+2 lados
+	 -numero de permutacoes de {1,2,...,n} tal que nao haja sequencias de 3 elementos consecutivos
+	 -numero de non crossing partitions do conjunto {1,...,n}
+	 - Hankel Matric eh uma matriz nxn tal que a posicao i,j contem o valor catalan(i+j-2). Essa matriz sempre terá determinante igual a 1.
+	 -trocando catalan(i+j-2) por catalan(i+j-1) o determinante continua sendo 1
+	 */
+
+//=============stars and bars=======================
+/*
+	-n identical objects into k labeled boxes is comb(n+k-1,n)
+	-equals combination with repetition of n+k-1, repeating n and k-1
+
+*/
+
+//================derrangements=======================
+/*
+	the number of ways to make an arrangment where no index is at its proper position is
+	derr(n) = (n-1)*(derr(n-1)+derr(n-2))
+	derr(0)=derr(1)=0
+
+*/
+
 const int ms=10000;
 ll c[ms][ms];
 
@@ -26,24 +56,3 @@ ll catalan(ll n, ll mod){ //import numtheory.cpp
 	return comb(2*n, n)*invmod(n+1,mod);
 }
 
-
-//========CATALAN NUMBERS=========
-/*
-	 -numero de Dyck words de tamanho 2n (strings com n X's e n Y's tal que nenhum prefixo tem mais X's do que Y's)
-	 -fazendo X==>'(' e Y==>')', catalan(n) é o numero de expressoes de parenteses válidas (com n pares de parenteses)
-	 -numero de maneiras de aplicar n operacoes binarias em n+1 fatores utilizando associatividade (numero de maneiras de parentizar n+1 fatores)
-	 -numero de arvores binarias com n+1 folhas
-	 -numero de caminhos monotonicos numa grid nxn
-	 -numero de maneiras de triangular polígono convexo de n+2 lados
-	 -numero de permutacoes de {1,2,...,n} tal que nao haja sequencias de 3 elementos consecutivos
-	 -numero de non crossing partitions do conjunto {1,...,n}
-	 - Hankel Matric eh uma matriz nxn tal que a posicao i,j contem o valor catalan(i+j-2). Essa matriz sempre terá determinante igual a 1.
-	 -trocando catalan(i+j-2) por catalan(i+j-1) o determinante continua sendo 1
-	 */
-
-//=============stars and bars=======================
-/*
-	-n identical objects into k labeled boxes is comb(n+k-1,n)
-	-equals combination with repetition of n+k-1, repeating n and k-1
-
-*/
