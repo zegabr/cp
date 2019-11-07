a62a70243543c298552f0f7d630acb5b3dd5f9fa
Jose Gabriel Silva Pereira <jgsp2@ug3c36.windows.cin.ufpe.br>
2019-11-07 09:42:16 -0300

    extended workig

--- math/extendedeuclids.cpp
+++ math/extendedeuclids.cpp
@@ -13,24 +13,33 @@ void euclides(ll a, ll b, ll &x, ll &y, ll &d) {
 	x = x1; y = y1;
 }
 
-bool anySolution(ll a, ll b, ll k, ll &x, ll &y, ll &d){
-	euclides(a,b,x,y,d);
-	if(k%d!=0) return 0; // no solution at all
-	x = k / d * x;
-	y = k / d * y;
-	return 1;
-}
-
 /////positive solution
 bool makePositive(ll &x,ll &y, ll d, ll a, ll b){
-	//recode this
+	if(x<0){
+		//x+ t*b/d >= 0 ==> t >= -x*d/b
+		ll t = ceil((double)-x*d/(double)b);
+		x += b/d * t;
+		y -= a/d * t;
+	}
+
+	if(y<0){
+		//y+ t*a/d >= 0 ==> t >= -x*d/a
+		ll t = ceil((double)-y*d/(double)a);
+		x -= b/d * t;
+		y += a/d * t;
+	}
+
+	return x>=0 and y>=0;
 }
+
 //ax+by=k
-int positiveSolution(ll a, ll b, ll k, ll &x, ll &y, ll &d){
+int solve(ll a, ll b, ll k, ll &x, ll &y, ll &d){
 	euclides(a,b,x,y,d);
 	if(k%d!=0) return 0; // no solution at all
 	x = k / d * x;
 	y = k / d * y;
-	if(makePositive(x,y,d,a,b)) return 2;//positive solution exists
+	if(makePositive(x,y,d,a,b)) return 2;//positive solution exists (comment this if any solution is good)
 	return 1;//integer solution exists
 }
+
+
