61012be5c2198dd7ba5a2bb1a6f5313b189b9be2
Jose Gabriel Silva Pereira <jgsp2@ug2c07.windows.cin.ufpe.br>
2019-11-27 08:03:38 -0300

    dsu classify

--- data-structures/dsu.cpp
+++ data-structures/dsu.cpp
@@ -1,20 +1,29 @@
 
-const int ms = 233333;
-int ds[ms],sz[ms];
-void makeset(int v=ms){
-	for(int i=0;i<v;i++){ ds[i]=i; sz[i]=1;}
-}
+class dsu{
+	vector<int> ds, sz;
+	public:	
+	dsu(int v){
+		sz.assign(v+1, 1);
+		ds.resize(v+1);
+		for(int i=0;i<=v;i++){ ds[i]=i;}
+	}
 
-int find(int i){//find
-	if(ds[i]!=i) ds[i]=find(ds[i]);
-	return i;
-}
+	int pai(int i){//find
+		if(ds[i]!=i) ds[i]=pai(ds[i]);
+		return ds[i];
+	}
 
-bool uni(int a, int b){//union
-	a=find(a), b=find(b);
-	if(a==b) return false;
-	if(sz[a]<sz[b])swap(a,b);
-	sz[a]+=sz[b];
-	ds[b]=a;
-	return true;
-}
+	bool uni(int a, int b){//union
+		a=pai(a), b=pai(b);
+		if(a==b) return false;
+		if(sz[a]<sz[b])swap(a,b);
+		sz[a]+=sz[b];
+		ds[b]=a;
+		return true;
+	}
+
+	int size(int a){
+		return sz[pai(a)];
+	}
+};
+
