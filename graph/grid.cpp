a49179b77a03a2529e1dbb54d912b80bc367b8ad
Jose Gabriel Silva Pereira <jgsp2@ug3c15.windows.cin.ufpe.br>
2019-11-29 16:37:06 -0300

    knights moves

--- graph/grid.cpp
+++ graph/grid.cpp
@@ -1,12 +1,13 @@
-
 //i grows from top->bottom
 //j grows from left->right
-int di[] = {-1, 0, 1, 0,-1, 1, 1, -1};
-int dj[] = {0, 1, 0, -1, 1, 1, -1, -1};
+//const int di[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+//const int dj[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+const int di[] = {-1, 0, 1, 0,-1, 1, 1, -1};//kings move
+const int dj[] = {0, 1, 0, -1, 1, 1, -1, -1};//kings move
 /*
 for(int k = 0; k < 8;k++){//u,r,d,l,ur,dr,dl,ul
-	int nx = di[k] + i;
-	int ny = dj[k] + j;
+	int ni = di[k] + i;
+	int ni = dj[k] + j;
 	//if safe then dfs(nx,ny)
 }
 */
