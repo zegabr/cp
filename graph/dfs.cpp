d52b7b0519ef73754ea353cceb58d694c58a1abd
zegabr <jgsp2@cin.ufpe.br>
2019-12-13 23:34:06 -0300

    dfs otimization

--- graph/dfs.cpp
+++ graph/dfs.cpp
@@ -2,7 +2,6 @@
 vector<int> vis(ms), g[ms];
 int tempo=1;//fazer tempo++ para cada vez que precisar zerar visitados
 void dfs(int u){//O(V+E)
-	if(vis[u]==tempo) return;
 	vis[u]=tempo;
 	for(auto v : g[u]){
 		if(vis[v]==tempo) continue;
