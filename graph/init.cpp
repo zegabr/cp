0c11101debe9214ea5f7bf46326bdff6c1728009
zegabr <jgsp2@cin.ufpe.br>
2020-01-05 22:05:59 -0300

    eds

--- graph/init.cpp
+++ graph/init.cpp
@@ -8,13 +8,14 @@ timer++ ==> desvisita todo mundo
 const int ms=2e7+5;
 const int inf = 1e8+5;
 vector<int> g[ms], dis(ms,inf), vis(ms); 
+vector<ii> eds;
 
 //PLS USE 0 INDEXED VERTICES
 void add(int u, int v){
   u--;v--;//if already 0-indexed, comment this line
   g[u].pb(v);
   g[v].pb(u);
-  //eds.pb({u,v});
+  eds.pb({u,v});
 }
 
 void cleargraph(){
