f80e056d11523024c04af51d43313bd2e1b7bfc7
zegabr <jgsp2@cin.ufpe.br>
2020-01-06 11:13:44 -0300

    definir ms na hr

--- graph/init.cpp
+++ graph/init.cpp
@@ -5,7 +5,7 @@ int timer=1;
    vis[u]==timer ==> u visitado
    timer++ ==> desvisita todo mundo
    */
-const int ms=2e7+5;
+const int ms= 
 const int inf = 1e8+5;
 vector<int> g[ms], dis(ms,inf), vis(ms); 
 vector<ii> eds;
@@ -15,7 +15,7 @@ void add(int u, int v){
   u--;v--;//if already 0-indexed, comment this line
   g[u].pb(v);
   g[v].pb(u);
-  eds.pb({u,v});
+  //  eds.pb({u,v});
 }
 
 void cleargraph(){
