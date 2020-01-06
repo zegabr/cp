dd36f145a0ea346d698ca96666da614c841cef8b
zegabr <jgsp2@cin.ufpe.br>
2020-01-06 12:35:40 -0300

    tsp

--- dp/tsp.cpp
+++ dp/tsp.cpp
@@ -1,16 +1,26 @@
+
+const int inf = 0x3f3f3f3f, ms = 11;
+int v, g[ms][ms], dp[ms][1<<ms], n;
+
 int tsp(int pos, int vis) {
-    if (vis+1 == (1<<n)){
-			return g[pos][n-1];
+  if (vis+1 == (1<<n)){
+    return g[pos][0];
+  }
+  if (dp[pos][vis] != -1) return dp[pos][vis];
+
+  dp[pos][vis] = inf;
+
+  for(int i=0;i<n;i++) {
+    if (!(vis & (1<<i))) {
+      dp[pos][vis] = min(dp[pos][vis], g[pos][i] + tsp(i, vis|(1<<i)));
     }
-		int &ans = dp[pos][vis];
-    if (ans != -1) return ans;
-    
-    int ans = 0x3f3f3f3f;
-    
-    for(int i=0;i<n;i++) {
-        if (!(vis & (1<<i))) {
-						ans = min(ans, g[pos][i] + tsp(i, vis|(1<<i)));
-        }
-    }
-    return ans;
+  }
+  return dp[pos][vis];
 }
+
+int solve(){
+  memset(dp,-1,sizeof dp);
+  return tsp(0,0);
+}
+
+
