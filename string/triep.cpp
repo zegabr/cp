007de8a65ba797d22b164ae793f44d4584389a75
zegabr <jgsp2@cin.ufpe.br>
2020-01-28 21:03:05 -0300

    basic operations added

--- string/triep.cpp
+++ string/triep.cpp
@@ -27,34 +27,105 @@ using namespace std;
 const int alfa = 26;
 
 class Node{  
-  char data;
-  bool isEnd;
-  int childrenCount;
-  Node *child[alfa];
-  
-  Node(){
-    for(int i=0;i<alfa;i++){
-      child[i]=(Node *)NULL;
+  public: 
+    char data;
+    int endHere;//count many insertions
+    int childrenCount;
+    Node *child[alfa];
+
+    Node(){
+      fill(child, child + alfa, (Node *)NULL);
+      endHere = 0;
+      childrenCount = 0;
     }
-  }
-  
 };
 
 class Trie{
-  
-  Trie(){
+  public:
+    Node *root;
 
-  }
+    Trie(){
+      root = new Node();
+    }
+    ~Trie(){
+      freeChildren(root);
+    }
+
+    void insert(string &s){
+      Node *cur = root;
+      root->childrenCount++;
+      for(char c : s){
+        int id = getId(c);
+        if(cur->child[id] == NULL){
+          cur->child[id] = new Node();
+        }
+        cur = cur->child[id];
+        cur->childrenCount++;
+      }
+      cur->endHere++;
+    }
+
+    int count(string &s){//count how many times s was inserted
+      Node *cur = root;
+      for(char c : s){
+        int id = getId(c);
+        if(cur->child[id] == NULL){
+          return 0;
+        }
+        cur = cur->child[id];
+      }
+      return cur->endHere;
+    }
+
+    int countWordsWithPrefix(string &prefix){
+      Node *cur = root;
+      for(char c : prefix){
+        int id = getId(c);
+        if(cur->child[id] == NULL){
+          return 0;
+        }
+        cur = cur->child[id];
+      }
+      return cur->childrenCount;
+    }
+
+    void removeOne(string &s){//remove one occurrence
+
+    }
+
+    void removeAll(string &s){//remove all occurrences
+
+    }
+
+  private:
+    int getId(char c){
+      return c-'a';
+    }
+
+    void freeChildren(Node *cur){
+      if(cur == NULL)
+        return;
+      for(Node *it : cur->child){
+        freeChildren(it);
+      }
+      delete cur;
+    }
 };
 
 
 
 
 int32_t main(){
-	ios::sync_with_stdio(0); cin.tie(0);
-  
+  Trie trie;
+  string s = "abacata";
+  trie.insert(s);
+  cout<<"abacata existe "<<trie.count(s)<<" vezes"<<endl;
+  s = "abacate";
+  trie.insert(s);
+  cout<<"abacate existe "<<trie.count(s)<<" vezes"<<endl;
 
-
+  s = "abaca";
+  cout<<trie.countWordsWithPrefix(s)<<endl;
 
 
 }
