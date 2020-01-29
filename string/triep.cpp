296bd7cfcfa4bfc0e8302bb3f07124bb9c9ff938
zegabr <jgsp2@cin.ufpe.br>
2020-01-29 18:06:07 -0300

    trie now accepts integers

--- string/triep.cpp
+++ string/triep.cpp
@@ -30,7 +30,7 @@ class Trie{
       root->childrenCount++;
       for(char c : s){
         int id = getId(c);
-        if(cur->child[id] == NULL){
+        if(!cur->child[id]){
           cur->child[id] = new Node();
         }
         cur = cur->child[id];
@@ -39,12 +39,26 @@ class Trie{
       cur->endHere++;
     }
 
-    int count(string &s, bool isPrefix = 0){
+    void insert(int &s){
+      Node *cur = root;
+      root->childrenCount++;
+      for(int i=0;i<32;i++){
+        int id = (s>>i)&1;
+        if(!cur->child[id]){
+          cur->child[id] = new Node();
+        }
+        cur = cur->child[id];
+        cur->childrenCount++;
+      }
+      cur->endHere++;
+    }
+
+    int count(string &s, bool isPrefix = false){
       //count how many times s was inserted or how many words has s as prefix
       Node *cur = root;
       for(char c : s){
         int id = getId(c);
-        if(cur->child[id] == NULL){
+        if(!cur->child[id]){
           return 0;
         }
         cur = cur->child[id];
@@ -53,21 +67,83 @@ class Trie{
       return cur->endHere;
     }
 
-    void remove(string &s, bool removeAll = false){//remove one or all occurrences
-      //TODO: use a stack of Node* to backtrack path and go back deleting the nodes with no children
+    int count(int &s, bool isPrefix = false){
+      //count how many times s was inserted or how many words has s as prefix
       Node *cur = root;
-      for(char c : s){
-        int id = getId(c);
-        if(cur->child[id] == NULL){
-          return;
+      for(int i=0;i<32;i++){
+        int id = (s>>i)&1;
+        if(!cur->child[id]){
+          return 0;
         }
         cur = cur->child[id];
       }
+      if(isPrefix) return cur->childrenCount;
+      return cur->endHere;
+
+
+    }
+    void remove(string &s, bool removeAll = false){//remove one or all occurrences
+      Node *cur = root;
+      vector<Node*> parent;
+      for(char c : s){
+        int id = getId(c);
+        if(!cur->child[id]) return;
+
+        cur = cur->child[id];
+        parent.pb(cur);
+      }
 
       cur->endHere = max(0,cur->endHere - 1);//remove one
-
       if(removeAll)
         cur->endHere = 0;//remove all
+
+      while(len(parent)){
+        cur = parent.back();
+        parent.ppb();
+        if(cur->endHere>0) 
+          continue;
+        bool hasChild=false;
+        for(int i=0;!hasChild and i<alfa;i++){
+          hasChild |= (cur->child[i]!=NULL);
+        }
+        if(!hasChild)
+          freeChildren(cur);
+      }
+    }
+
+    void remove(int &s, bool removeAll = false){
+      Node *cur = root;
+      vector<Node*> parent;
+      for(int i=0;i<32;i++){
+        int id = (s>>i)&1;
+        if(!cur->child[id]) return;
+
+        cur = cur->child[id];
+        parent.pb(cur);
+      }
+
+      cur->endHere = max(0,cur->endHere - 1);//remove one
+      if(removeAll)
+        cur->endHere = 0;//remove all
+
+      while(len(parent)){
+        cur = parent.back();
+        parent.ppb();
+        if(cur->endHere>0) 
+          continue;
+        bool hasChild=false;
+        for(int i=0;!hasChild and i<alfa;i++){
+          hasChild |= (cur->child[i]!=NULL);
+        }
+        if(!hasChild)
+          freeChildren(cur);
+      }
+    }
+
+    int solve(int n){
+      int res = n;
+
+      return res;
     }
 
   private:
@@ -76,7 +152,7 @@ class Trie{
     }
 
     void freeChildren(Node *cur){
-      if(cur == NULL)
+      if(!cur)
         return;
       for(Node *it : cur->child){
         freeChildren(it);
