d0936595942494b00ededc4a4ccc5edce6666bc0
zegabr <jgsp2@cin.ufpe.br>
2020-02-20 10:20:20 -0300

    refactor of r.sh use

--- STUFF/r.sh
+++ STUFF/r.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 #r de reset
 
+cp ./cp/STUFF/Makefile ./
 
 
-cp tmp.cpp x.cpp
-cp pmp.py p.py
+cp ./cp/STUFF/tmp.cpp ./x.cpp
+cp ./cp/STUFF/pmp.py ./p.py
 echo "x.cpp and p.py reseted"
 touch i
 
