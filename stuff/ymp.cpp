dcfa862a608fe946817cc21590461c6bf772a3aa
zegabr <jgsp2@cin.ufpe.br>
2020-03-06 11:42:10 -0300

    adding testcase template

---
+++ stuff/ymp.cpp
@@ -0,0 +1,40 @@
+#include <set>
+#include <map>
+#include <cmath>
+#include <queue>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define x first
+#define y second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long
+#define ld long double
+#define ii pair<int,int>
+#define heap priority_queue
+#define ull unsigned long long
+#define all(k) k.begin(),k.end()
+#define rall(k) k.rbegin(),k.rend()
+#define dbg(x) cerr<<#x<<" = "<<x<<endl
+#define len(v) ((int)v.size())
+#define fa(a,c) for(auto &a:c)
+#define endl '\n'
+
+
+void ze(){
+
+  
+}
+
+
+int32_t main(){
+	ios::sync_with_stdio(0); cin.tie(0);
+  int tc; 
+  cin>>tc;
+  while(tc--) ze();
+}
