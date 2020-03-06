4e31f6309597e3be662bc3ab193f10db064102d1
zegabr <jgsp2@cin.ufpe.br>
2020-03-06 11:32:54 -0300

    renaming stuff

--- stuff/r.sh
+++ stuff/r.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 #r de reset
 
-cp ./cp/STUFF/Makefile ./
+cp ./cp/stuff/Makefile ./
 
 
-cp ./cp/STUFF/tmp.cpp ./x.cpp
-cp ./cp/STUFF/pmp.py ./p.py
+cp ./cp/stuff/tmp.cpp ./x.cpp
+cp ./cp/stuff/pmp.py ./p.py
 echo "x.cpp and p.py reseted"
 touch i
 
