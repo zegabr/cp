78507a5bb1ac60022fdc97ab2f8504e11031ec7f
zegabr <jgsp2@cin.ufpe.br>
2020-03-06 11:46:03 -0300

    corrected

--- stuff/r.sh
+++ stuff/r.sh
@@ -5,7 +5,7 @@ cp ./cp/stuff/Makefile ./
 
 
 cp ./cp/stuff/tmp.cpp ./x.cpp
+cp ./cp/stuff/ymp.cpp ./u.cpp
 cp ./cp/stuff/pmp.py ./p.py
-echo "x.cpp and p.py reseted"
-touch i
+echo "x.cpp, u.cpp and  p.py reseted"
 
