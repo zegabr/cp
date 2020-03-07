22b308ca3efbcc681935db82e71f0ab9f9a52409
zegabr <jgsp2@cin.ufpe.br>
2020-03-07 12:51:01 -0300

    string

--- stuff/r.sh
+++ stuff/r.sh
@@ -6,5 +6,7 @@ cp ./cp/stuff/Makefile ./
 
 cp ./cp/stuff/tmp.cpp ./x.cpp
 cp ./cp/stuff/pmp.py ./p.py
-echo "x.cpp and p.py reseted"
+echo
+echo "==========x.cpp and p.py reseted============"
+echo
 
