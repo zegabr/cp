2a28f5488a85c784b0b9d20e1494f522b2e16398
zegabr <jgsp2@cin.ufpe.br>
2020-03-09 13:59:13 -0300

    better for leetcode

--- graph/init.cpp
+++ graph/init.cpp
@@ -4,16 +4,17 @@ int timer=1;
    vis[u]<timer ==> u nao visitado
    vis[u]==timer ==> u visitado
    timer++ ==> desvisita todo mundo
-   */
-const int ms= 10;
+
+*/
+const int ms= 300000;
 const int inf = 1e8+5;
-vector<int> dis(ms,inf), vis(ms); 
-vector<vector<int>> g(ms);
+vector<int> dis, vis; 
+vector<vector<int>> g;
 vector<ii> eds;
 
 //PLS USE 0 INDEXED VERTICES
 void add(int u, int v){
-  u--;v--;//if already 0-indexed, comment this line
+  u--;v--;//if already 0-indexed, delete this line
   g[u].pb(v);
   g[v].pb(u);
   //  eds.pb({u,v});
@@ -22,5 +23,6 @@ void add(int u, int v){
 void cleargraph(){
   g.assign(ms, vector<int>(0));
   dis.assign(ms,inf);
+  if(vis.size()==0) vis.assign(ms,0);
 }
 
