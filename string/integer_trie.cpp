bc87501767091ab8b937778832d6751d22bf8f02
zegabr <jgsp2@cin.ufpe.br>
2020-03-15 11:26:53 -0300

    cache optimization

--- string/integer_trie.cpp
+++ string/integer_trie.cpp
@@ -3,97 +3,97 @@
 #define alfa 2
 
 class Trie{
-  public:
-    vector<vector<int>> trie;
-    vector<int> word;
-    vector<int> pref;
-    vector<int> data;//used for debbug and removal
-    int z;
+    public:
+        vector<vector<int>> trie;
+        vector<int> word;
+        vector<int> pref;
+        vector<int> data;//used for debbug and removal
+        int z;
 
-    Trie() {
-      trie.assign(maxsize,vector<int>(alfa,0));
-      word.assign(maxsize,0);
-      pref.assign(maxsize,0);
-      data.assign(maxsize,0);
-      z = 1;
-    }
-
-    void insert(int &s) {
-      int cur = 0, id;
-      pref[cur]++;
-      for(int i = 31; i >= 0;i--) {
-        id = getid(s,i);
-        if(!trie[cur][id]) {
-          reset(z);
-          trie[cur][id] = z++;
+        Trie() {
+            trie.assign(alfa,vector<int>(maxsize,0));
+            word.assign(maxsize,0);
+            pref.assign(maxsize,0);
+            data.assign(maxsize,0);
+            z = 1;
         }
-        cur = trie[cur][id];
-        data[cur]=id;
-        pref[cur]++;
-      }
-      word[cur]++;
-    }
 
-    int count(int &s) {
-      int cur = 0, id;
-      for(int i = 31; i >= 0; i--) {
-        id = getid(s,i);
-        if(!trie[cur][id]) return 0;
-        cur = trie[cur][id];
-      }
-      return word[cur];
-    }
-
-    void remove(int &s, bool removeAll=false){
-      //remove one or all ocurrences of s
-      int cur = 0, id;
-      vector<int> parentstack;
-      parentstack.pb(cur);
-      for(int i = 31; i >= 0; i--) {
-        id = getid(s,i);
-        if(!trie[cur][id]) return;
-        cur = trie[cur][id];
-        parentstack.pb(cur);
-      }
-
-      int toRemove = removeAll ? word[cur] : 1;
-      word[cur] -= toRemove;
-
-      while(len(parentstack)>1){
-        cur = parentstack.back();
-        parentstack.ppb();
-        pref[cur] -= toRemove;
-
-        if(pref[cur]){
-          continue;
-        } else{
-          //not a prefix, hence has no child
-          trie[parentstack.back()][data[cur]]=0;
+        void insert(int &s) {
+            int cur = 0, id;
+            pref[cur]++;
+            for(int i = 31; i >= 0;i--) {
+                id = getid(s,i);
+                if(!trie[id][cur]) {
+                    reset(z);
+                    trie[id][cur] = z++;
+                }
+                cur = trie[id][cur];
+                data[cur]=id;
+                pref[cur]++;
+            }
+            word[cur]++;
         }
-      }
-    }
 
-    int maximizeXorWith (int &s){
-      int res = 0, id, cur=0;
-      for(int i=31;i>=0;i--){
-        id = getid(s,i);
-        if(trie[cur][!id]){
-          res += (1<<i);
-          cur = trie[cur][!id];
-        }else{
-          cur = trie[cur][id];
+        int count(int &s) {
+            int cur = 0, id;
+            for(int i = 31; i >= 0; i--) {
+                id = getid(s,i);
+                if(!trie[id][cur]) return 0;
+                cur = trie[id][cur];
+            }
+            return word[cur];
         }
-      }
-      return res;
-    }
 
-  private:
-    void reset(int node){
-      trie[node].assign(alfa,0);
-      pref[node]=word[node]=0;
-    }
+        void remove(int &s, bool removeAll=false){
+            //remove one or all ocurrences of s
+            int cur = 0, id;
+            vector<int> parentstack;
+            parentstack.pb(cur);
+            for(int i = 31; i >= 0; i--) {
+                id = getid(s,i);
+                if(!trie[id][cur]) return;
+                cur = trie[id][cur];
+                parentstack.pb(cur);
+            }
 
-    int getid(int &s, int &i) {
-      return (s>>i)&1;
-    }
+            int toRemove = removeAll ? word[cur] : 1;
+            word[cur] -= toRemove;
+
+            while(len(parentstack)>1){
+                cur = parentstack.back();
+                parentstack.ppb();
+                pref[cur] -= toRemove;
+
+                if(pref[cur]){
+                    continue;
+                } else{
+                    //not a prefix, hence has no child
+                    trie[data[cur]][parentstack.back()]=0;
+                }
+            }
+        }
+
+        int maximizeXorWith (int &s){
+            int res = 0, id, cur=0;
+            for(int i=31;i>=0;i--){
+                id = getid(s,i);
+                if(trie[!id][cur]){
+                    res += (1<<i);
+                    cur = trie[!id][cur];
+                }else{
+                    cur = trie[id][cur];
+                }
+            }
+            return res;
+        }
+
+    private:
+        void reset(int node){
+            trie[node].assign(alfa,0);
+            pref[node]=word[node]=0;
+        }
+
+        int getid(int &s, int &i) {
+            return (s>>i)&1;
+        }
 };
--- string/trie.cpp
+++ string/trie.cpp
@@ -3,84 +3,84 @@
 #define alfa 26
 
 class Trie{
-  public:
-    vector<vector<int>> trie;
-    vector<int> isword;
-    vector<int> pref;
-    vector<int> data;//used for debugging and removals
-    int z;
+    public:
+        vector<vector<int>> trie;
+        vector<int> isword;
+        vector<int> pref;
+        vector<int> data;//used for debugging and removals
+        int z;
 
-    Trie() {
-      trie.assign(maxsize,vector<int>(alfa,0));
-      isword.assign(maxsize,0);
-      pref.assign(maxsize,0);
-      data.assign(maxsize,0);
-      z = 1;
-    }
-
-    void insert(string &s) {
-      int cur = 0, id;
-      pref[cur]++;
-      for(int i = 0; i < len(s); i++) {
-        id = getid(s[i]);
-        if(!trie[cur][id]) {
-          reset(z);
-          trie[cur][id] = z++;
+        Trie() {
+            trie.assign(alfa,vector<int>(maxsize,0));
+            isword.assign(maxsize,0);
+            pref.assign(maxsize,0);
+            data.assign(maxsize,0);
+            z = 1;
         }
-        cur = trie[cur][id];
-        data[cur]=id;
-        pref[cur]++;
-      }
-      isword[cur]++;
-    }
 
-    int count(string &s, bool prefixQuery=false) {
-      //count final words or common prefixes
-      int cur = 0, id;
-      for(int i = 0; i < len(s); i++) {
-        id = getid(s[i]);
-        if(!trie[cur][id]) return 0;
-        cur = trie[cur][id];
-      }
-      return prefixQuery ? pref[cur] : isword[cur];
-    }
-
-    void remove(string &s, bool removeAll=false){
-      //remove one or all ocurrences of s
-      int cur = 0, id;
-      vector<int> parentstack;
-      parentstack.pb(cur);
-      for(int i = 0; i < len(s); i++) {
-        id = getid(s[i]);
-        if(!trie[cur][id]) return;
-        cur = trie[cur][id];
-        parentstack.pb(cur);
-      }
-
-      int toRemove = removeAll ? isword[cur] : 1;
-      isword[cur] -= toRemove;
-
-      while(len(parentstack)>1){
-        cur = parentstack.back();
-        parentstack.ppb();
-        pref[cur] -= toRemove;
-
-        if(pref[cur]){
-          continue;
-        } else{
-          //not a prefix, hence has no child
-          trie[parentstack.back()][data[cur]]=0;
+        void insert(string &s) {
+            int cur = 0, id;
+            pref[cur]++;
+            for(int i = 0; i < len(s); i++) {
+                id = getid(s[i]);
+                if(!trie[id][cur]) {
+                    reset(z);
+                    trie[id][cur] = z++;
+                }
+                cur = trie[id][cur];
+                data[cur]=id;
+                pref[cur]++;
+            }
+            isword[cur]++;
         }
-      }
-    }
 
-  private:
-    void reset(int node){
-      trie[node].assign(alfa,0);
-      pref[node]=isword[node]=0;
-    }
+        int count(string &s, bool prefixQuery=false) {
+            //count final words or common prefixes
+            int cur = 0, id;
+            for(int i = 0; i < len(s); i++) {
+                id = getid(s[i]);
+                if(!trie[id][cur]) return 0;
+                cur = trie[id][cur];
+            }
+            return prefixQuery ? pref[cur] : isword[cur];
+        }
 
-    int getid(char c) {
-      return c - 'a';
-    }
+        void remove(string &s, bool removeAll=false){
+            //remove one or all ocurrences of s
+            int cur = 0, id;
+            vector<int> parentstack;
+            parentstack.pb(cur);
+            for(int i = 0; i < len(s); i++) {
+                id = getid(s[i]);
+                if(!trie[id][cur]) return;
+                cur = trie[id][cur];
+                parentstack.pb(cur);
+            }
+
+            int toRemove = removeAll ? isword[cur] : 1;
+            isword[cur] -= toRemove;
+
+            while(len(parentstack)>1){
+                cur = parentstack.back();
+                parentstack.ppb();
+                pref[cur] -= toRemove;
+
+                if(pref[cur]){
+                    continue;
+                } else{
+                    //not a prefix, hence has no child
+                    trie[data[cur]][parentstack.back()]=0;
+                }
+            }
+        }
+
+    private:
+        void reset(int node){
+            trie[node].assign(alfa,0);
+            pref[node]=isword[node]=0;
+        }
+
+        int getid(char c) {
+            return c - 'a';
+        }
 };
