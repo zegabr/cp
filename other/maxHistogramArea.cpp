97839e24f010c1fb571bd3be7cbd8f05aac010b4
José Gabriel <jgsp2@cin.ufpe.br>
2020-03-26 14:53:16 -0300

    Update maxHistogramArea.cpp

--- other/maxHistogramArea.cpp
+++ other/maxHistogramArea.cpp
@@ -3,20 +3,18 @@ int largestRectangleArea(vector<int>& H) {
         int n = H.size();
         int res = 0;
         pos.push(-1);
-        for(int i=0;i<H.size();i++){
+        for(int i=0;i<n;i++){
             while(pos.top()!=-1 and H[i]<H[pos.top()]){
-                int r = i;
-                int l = pos.top(); pos.pop();
-                res = max(res, H[l]*(r-pos.top()-1));
+                int h = H[pos.top()]; 
+                pos.pop();
+                res = max(res, h*( (i-1) - pos.top()));
             }
             pos.push(i);
         }
-        
         while(pos.top()!=-1){
-                int r = n;
-                int l = pos.top(); pos.pop();
-                res = max(res, H[l]*(r-pos.top()-1));
+            int h = H[pos.top()]; 
+            pos.pop();
+            res = max(res, h*( (n-1) - pos.top()));
         }
-        
         return res;
     }
