2a7c56f92113d9e531cceabc82e478fa602eb0ef
José Gabriel <jgsp2@cin.ufpe.br>
2020-04-13 18:21:13 -0300

    Update item.cpp

--- other/item.cpp
+++ other/item.cpp
@@ -1,9 +1,17 @@
 
 //modifiable class
 class item {
-	public:
-	int x, y;
-	bool operator<(const item &o) const {
+public:
+  int x;
+  int y;
+  
+  item(int a, int b){//constructor 
+		//instantiate as auto obj = item(12,321) or item obj(12,321) 
+    x=a;
+    y=b;
+  }
+  
+	bool operator<(const item &o) const {//use this if is using a heap
 		return x < o.x || x == o.x && y < o.y;
 	}
 };
