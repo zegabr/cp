bf1cbd5e835dfcbd8db4cd43d1b9b2834de3bdc9
zegabr <jgsp2@cin.ufpe.br>
2020-04-19 23:47:16 -0300

    2-SAT at kosaraju.cpp

--- graph/init.cpp
+++ graph/init.cpp
@@ -1,30 +1,25 @@
-
 //---------------------------
-int timer=1;
 /*
-   vis[u]<timer ==> u nao visitado
-   vis[u]==timer ==> u visitado
-   timer++ ==> desvisita todo mundo
-
-*/
+   vis[u]<timer ==> u has not been visited
+   vis[u]==timer ==> u has been visited
+   timer++ ==> unvisit every node
+   */
+int timer=1;
 const int ms= 300000;
 const int inf = 1e8+5;
 vector<int> dis, vis; 
 vector<vector<int>> g;
 vector<ii> eds;
-//----------------------------
 
-//PLS USE 0 INDEXED VERTICES
-void add(int u, int v){
-  u--;v--;//if already 0-indexed, delete this line
+void add(int u, int v, bool bi=0){ //PLS USE 0 INDEXED VERTICES
   g[u].pb(v);
-  g[v].pb(u);
-  //  eds.pb({u,v});
+  if(bi) g[v].pb(u);
+  //eds.pb({u,v});
 }
 
-void cleargraph(int n = ms){
+void reset_graph(int n = ms){
   g.assign(n, vector<int>());
   dis.assign(n,inf);
   if(vis.empty()) vis.assign(n,0);
 }
-
+//----------------------------
--- graph/kosaraju.cpp
+++ graph/kosaraju.cpp
@@ -1,54 +1,68 @@
 
 //for directed graphs
-//pls use 0 indexed vertices
-//import ./init.cpp
-queue<int> order;
-vector<int> comp;
-vector<vector<int>> sccs;
+//PLS USE 0 INDEXED VERTICES
+queue<int> order;//store topological reversal order
+vector<int> comp;//store components
+vector<int> assignment;//store 2-SAT solution
 
 void init(int n){
-  cleargraph(n);//from ./init.cpp
+  reset_graph(n);//from ./init.cpp
   while(order.size())
     order.pop();
   timer++;
-  sccs.clear();
   eds.clear();
   comp.assign(n,-1);
 }
 
 void dfs1(int u){
-  vis[u]=timer;
+  vis[u] = timer;
   for(auto v : g[u]){
-    if(vis[v]<timer){
+    if(vis[v] < timer){
       dfs1(v);
     }
   }
   order.push(u);
 }
 
-void dfs2(int u, int component){
-  vis[u]=timer;
-  comp[u]=component;
+void dfs2(int u, int c){
+  vis[u] = timer;
+  comp[u] = c;
   for(auto v : g[u]){
-    if(vis[v]<timer){
-      dfs2(v, component);
+    if(vis[v] < timer){
+      dfs2(v, c);
     }
   }
 }
 
-void getsccs(int n){
+void add_clause(int a, int b){//a and b has to be already at form 2k or 2k+1
+  //a V b
+  add(a^1, b);//not( a) => b
+  add(b^1, a);//not( b) => a
+}
+
+bool scc(int n){//get sccs and solve 2-SAT (n has to be 2 times number of variables)
   timer++;
-  for(int i=0;i<n;i++)
-    if(vis[i]<timer) 
+  for(int i = 0; i < n; i++)
+    if(vis[i] < timer) 
       dfs1(i);
 
-
   timer++;
+  int j = 0;
   while(order.size()){
     int u = order.front();
     order.pop();
-    if(vis[u]<timer) 
-      dfs2(u,order.size());//order.size will be the number of the component
+    if(vis[u] < timer) 
+      dfs2(u, j++);//j will be the number of the component
   }
-}
 
+  if(n%2==0){//solving 2-SAT here
+    assignment.assign(n/2, false);
+    for(int i=0;i<n;i+=2){
+      if(comp[i] == comp[i+1])
+        return false;
+      assignment[i/2] = comp[i] > comp[i+1];
+    }
+  }
+  return true;
+
+}
