dd21bbb390e501b4520e35d14352a862e38b453e
zegabr <jgsp2@cin.ufpe.br>
2020-04-27 14:13:57 -0300

    line number reduction

--- data-structures/heap.cpp
+++ data-structures/heap.cpp
@@ -1,6 +1,4 @@
-template<class T>
-using maxheap = std::priority_queue<T>;
-template<class T>
-using minheap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+template<class T> using maxheap = std::priority_queue<T>;
+template<class T> using minheap = std::priority_queue<T, std::vector<T>, std::greater<T>>;
 
 
