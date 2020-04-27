92a1636490dedce06e1a6fb5293a5685505b569c
zegabr <jgsp2@cin.ufpe.br>
2020-04-27 14:37:19 -0300

    refactor of tmp.cpp

--- string/hash.cpp
+++ string/hash.cpp
@@ -1,4 +1,5 @@
 
+#define ull unsigned long long
 #define Hash pair<ll,ull>
 const ll base = 137;//cover all ascii values
 const ll mod = (1ll<<31)-1ll;
@@ -8,49 +9,49 @@ vector<ll> pot1;
 vector<ull> pot2;
 
 void buildpot(int maxsize=2000000){//call at beginning of main
-  pot1.resize(maxsize);
-  pot2.resize(maxsize);
-  pot1[0] = pot2[0] = 1;
-  for(int i = 1;i < maxsize;i++){
-    pot1[i] = pot1[i-1] * base % mod;
-    pot2[i] = pot2[i-1] * base;
-  }   
+    pot1.resize(maxsize);
+    pot2.resize(maxsize);
+    pot1[0] = pot2[0] = 1;
+    for(int i = 1;i < maxsize;i++){
+        pot1[i] = pot1[i-1] * base % mod;
+        pot2[i] = pot2[i-1] * base;
+    }   
 }
 
 void build(string &s, vector<ll> &h, vector<ull> &h2){
-  int n = s.size();
-  h.resize(n);
-  h2.resize(n);
-  h[0] = h2[0] = s[0];
-  for(int i = 1;i < n;i++){
-    h[i] = (h[i-1] * base % mod + s[i]) % mod;
-    h2[i] = h2[i-1] * base + s[i];
-  }
+    int n = s.size();
+    h.resize(n);
+    h2.resize(n);
+    h[0] = h2[0] = s[0];
+    for(int i = 1;i < n;i++){
+        h[i] = (h[i-1] * base % mod + s[i]) % mod;
+        h2[i] = h2[i-1] * base + s[i];
+    }
 }
 
 //hash function is a double hash
 Hash get(int l, int r, vector<ll> &h, vector<ull> &h2, bool isreversed=0){
-  int n = h.size();
-  if(isreversed){
-    int R=r,L=l;
-    r = n-L-1;
-    l = n-R-1;
-  }
-  ll res1 = h[r];
-  ull res2 = h2[r];
-  if(l>0){
-    res1 = (res1 - pot1[r-l+1]*h[l-1]%mod + mod)%mod;
-    res2 = res2 - pot2[r-l+1] * h2[l-1];
-  } 
-  return Hash(res1,res2);
+    int n = h.size();
+    if(isreversed){
+        int R=r,L=l;
+        r = n-L-1;
+        l = n-R-1;
+    }
+    ll res1 = h[r];
+    ull res2 = h2[r];
+    if(l>0){
+        res1 = (res1 - pot1[r-l+1]*h[l-1]%mod + mod)%mod;
+        res2 = res2 - pot2[r-l+1] * h2[l-1];
+    } 
+    return Hash(res1,res2);
 }
 
 Hash getpref(int m,vector<ll> &h, vector<ull> &h2){
-  return get(0,m-1,h,h2);
+    return get(0,m-1,h,h2);
 }
 
 Hash getsuff(int m, vector<ll> &h, vector<ull> &h2){
-  int n = h.size();
-  return get(n-m,n-1,h,h2);
+    int n = h.size();
+    return get(n-m,n-1,h,h2);
 }
 
--- stuff/tmp.cpp
+++ stuff/tmp.cpp
@@ -16,29 +16,32 @@ using namespace std;
 #define ppf pop_front
 #define ll long long
 #define ld long double
-#define ii pair<int,int>
-#define ull unsigned long long
-#define all(k) k.begin(),k.end()
+#define sz(v) ((int)v.size())
 #define fa(a,c) for(auto &a : c)
+#define all(k) k.begin(),k.end()
 #define rall(k) k.rbegin(),k.rend()
+#define no cout<<"NO"<<endl
+#define yes cout<<"YES"<<endl
 #define db(x) cerr<<#x<<" = "<<x<<endl
-#define CT(k) cout<<"Case #"<<k<<": "
-#define len(v) ((int)v.size())
+#define codejam(k) cout<<"Case #"<<k<<": "
 #define endl '\n'
 
 
 
-void solve(int caso=0){
+
+
+void main2(int caso=0){
 
 
 }
 int32_t main(){
-  ios::sync_with_stdio(0); cin.tie(0);
-  int t; 
+    ios::sync_with_stdio(0); cin.tie(0);
+    cout<<setprecision(7)<<endl;
+    int t; 
 #ifdef TESTCASE
-  cin>>t;
+    cin>>t;
 #else 
-  t = 1;
+    t = 1;
 #endif
-  for(int i=1;i<=t;i++) solve(i);
+    for(int i=1;i<=t;i++) main2(i);
 }
