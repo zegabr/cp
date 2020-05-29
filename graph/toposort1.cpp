977d123e4990fa8c49e8ac4268f87480b9de154e
zegabr <jgsp2@cin.ufpe.br>
2020-05-29 14:44:32 -0300

    toposort 2, dfs recursive

---
+++ graph/toposort1.cpp
@@ -0,0 +1,15 @@
+
+vector<int> dg(ms);//entry degree
+vector<int> ans;
+
+void toposort(int n){//O((V+E)logV)
+    priority_queue<int, vector<int>,greater<int>> pq;
+    for(int i=0;i<n;i++)if(!dg[i])pq.push(i);
+    while(pq.size()){
+        int u = pq.top(); pq.pop();
+        for(auto v : g[u]){
+            if(--dg[v]==0) pq.push(v);
+        }
+        ans.push_back(u);
+    }
+}
