02291c0561c20e3a02060a6af8c0e0c7c0e2804f
zegabr <jgsp2@cin.ufpe.br>
2020-06-05 16:43:33 -0300

    fix spacing

--- data-structures/dsu.cpp
+++ data-structures/dsu.cpp
@@ -5,25 +5,25 @@ class Dsu{
     Dsu(int v){
         sz.assign(v+1, 1);
         ds.resize(v+1);
-        for(int i=0;i<=v;i++){ ds[i]=i;}
+        for(int i=0; i <= v;i++) ds[i] = i;
     }
 
     int pai(int i){//find
-        if(ds[i]!=i) ds[i]=pai(ds[i]);
+        if(ds[i] != i) ds[i] = pai(ds[i]);
         return ds[i];
     }
 
     bool uni(int a, int b){//union
-        a=pai(a), b=pai(b);
-        if(a==b) return false;
-        if(sz[a]<sz[b])swap(a,b);
-        sz[a]+=sz[b];
-        ds[b]=a;
+        a = pai(a), b = pai(b);
+        if(a == b) return false;
+        if(sz[a] < sz[b])swap(a,b);
+        sz[a] += sz[b];
+        ds[b] = a;
         return true;
     }
 
     bool same(int a, int b){
-        return pai(a)==pai(b);
+        return pai(a) == pai(b);
     }
 
     int size(int a){
