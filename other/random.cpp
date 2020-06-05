da5ff3b6a5d5d8ea01622f7537a458aa809e5658
José Gabriel <jgsp2@cin.ufpe.br>
2020-06-05 15:30:19 -0300

    added random for leetcode

--- other/random.cpp
+++ other/random.cpp
@@ -20,4 +20,12 @@ inline double random_double(int i, int j){
 // bernoulli_distribution, binomial_distribution, geometric_distribution
 // normal_distribution, poisson_distribution, exponential_distribution
 
+//========================outro random (funciona no leetcode)==============
+void reset_seed(){
+    srand(unsigned(time(NULL)));
+}
+
+int randint(int l, int r){//random from l to r inclusive
+    return rand() % (r+1) + l;   
+}
 
