af40aac03938f097578ba56e0341e8a6a9255051
zegabr <jgsp2@cin.ufpe.br>
2020-06-17 11:33:49 -0300

    grid moves refactor

--- graph/grid.cpp
+++ graph/grid.cpp
@@ -1,14 +1,3 @@
-//i grows from top->bottom
-//j grows from left->right
-vector<int> ci = {-2, -2, -1, -1,  1,  1,  2,  2};// Knights Move
-vector<int> cj = {-1,  1, -2,  2, -2,  2, -1,  1};// Knights Move
 
-vector<int> ri = {-1, 0, 1, 0,-1, 1, 1, -1};//kings move
-vector<int> rj = {0, 1, 0, -1, 1, 1, -1, -1};//kings move
-/*
-for(int k = 0; k < 8;k++){//u,r,d,l,ur,dr,dl,ul
-	int ni = ri[k] + i;
-	int ni = rj[k] + j;
-	//if safe then dfs(nx,ny)
-}
-*/
+const vector<vector<int>> kingsMoves = {{-1,0},{0,1},{1,0},{0,-1},{-1,1},{1,1},{1,-1},{-1,-1}};//4 adj + 4 diags
+const vector<vector<int>> knightsMoves = {{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,-1},{2,1}};
