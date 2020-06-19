88c67edd44feaf1e8dba70688f2f6f473da6a172
zegabr <jgsp2@cin.ufpe.br>
2020-06-19 15:42:43 -0300

    refactor(string hash): static pot1 and pot2

--- string/hash.cpp
+++ string/hash.cpp
@@ -1,31 +1,51 @@
 
 typedef pair<long long, unsigned long long> Hash;
-const long long base = 137;//cover all ascii values
-const unsigned long long mod = (1ll<<31)-1ll;
-//mod2 = 1<<64
 
 class Hasher{
     private:
+        const long long base = 137;//cover all ascii values
+        const unsigned long long mod = (1ll<<31)-1ll; //mod2 = 1<<64
+
         vector<long long> h1;
         vector<unsigned long long> h2;
         bool isReversed;
-    
-        vector<long long> pot1;//be aware of MLE, maybe put pot1 and pot2 as global
-        vector<unsigned long long> pot2;
-        void buildpot(int maxsize = 2000000){
+
+        void buildpot1(vector<long long> & pot1, int maxsize = 2000000){
+            int last = max(1,(int)pot1.size());
             pot1.resize(maxsize);
-            pot2.resize(maxsize);
-            pot1[0] = pot2[0] = 1;
-            for(int i = 1;i < maxsize;i++){
+            pot1[0] = 1;
+            for(int i = last;i < maxsize;i++){
                 pot1[i] = pot1[i-1] * base % mod;
+            }   
+        }
+
+        void buildpot2(vector<unsigned long long> & pot2, int maxsize = 2000000){
+            int last = max(1,(int)pot2.size());
+            pot2.resize(maxsize);
+            pot2[0] = 1;
+            for(int i = last;i < maxsize;i++){
                 pot2[i] = pot2[i-1] * base;
             }   
+        }
+
+        long long getpot1(int i){
+            static vector<long long> pot1;
+            if(i > pot1.size()){
+                buildpot1(pot1, i+1);
+            }
+            return pot1[i];
+        }
+
+        unsigned long long getpot2(int i){
+            static vector<unsigned long long> pot2;
+            if(i > pot2.size()){
+                buildpot2(pot2, i+1);
+            }
+            return pot2[i];
         }
     public:
         Hasher(string &s, bool reversed = false){
             isReversed = reversed;
-            if(pot1.empty())
-                buildpot(s.size());
             int n = s.size();
             h1.resize(n);
             h2.resize(n);
@@ -46,8 +66,8 @@ class Hasher{
             long long res1 = h1[r];
             unsigned long long res2 = h2[r];
             if(l>0){
-                res1 = (res1 - pot1[r-l+1]*h1[l-1]%mod + mod)%mod;
-                res2 = res2 - pot2[r-l+1] * h2[l-1];
+                res1 = (res1 - getpot1(r-l+1)*h1[l-1]%mod + mod)%mod;
+                res2 = res2 - getpot2(r-l+1) * h2[l-1];
             } 
             return Hash(res1,res2);
         }
@@ -60,7 +80,7 @@ class Hasher{
             int n = h1.size();
             return get(n-m,n-1);
         }
-    
+
         int size(){
             return h1.size();
         }
