59504728825ad0f126a63ae2c710ad7f7ebac802
zegabr <jgsp2@cin.ufpe.br>
2020-06-24 18:21:47 -0300

    add linear catalan

--- math/combinatorics.cpp
+++ math/combinatorics.cpp
@@ -1,19 +1,4 @@
 
-
-//========CATALAN NUMBERS=========
-/*
-   -numero de Dyck words de tamanho 2n (strings com n X's e n Y's tal que nenhum prefixo tem mais X's do que Y's)
-   -fazendo X==>'(' e Y==>')', catalan(n) é o numero de expressoes de parenteses válidas (com n pares de parenteses)
-   -numero de maneiras de aplicar n operacoes binarias em n+1 fatores utilizando associatividade (numero de maneiras de parentizar n+1 fatores)
-   -numero de arvores binarias com n+1 folhas
-   -numero de caminhos monotonicos numa grid nxn
-   -numero de maneiras de triangular polígono convexo de n+2 lados
-   -numero de permutacoes de {1,2,...,n} tal que nao haja sequencias de 3 elementos consecutivos
-   -numero de non crossing partitions do conjunto {1,...,n}
-   - Hankel Matric eh uma matriz nxn tal que a posicao i,j contem o valor catalan(i+j-2). Essa matriz sempre terá determinante igual a 1.
-   -trocando catalan(i+j-2) por catalan(i+j-1) o determinante continua sendo 1
-   */
-
 //=============stars and bars=======================
 /*
    -n identical objects into k labeled boxes is comb(n+k-1,n)
@@ -50,14 +35,5 @@ void pascaltriangle(int n){
         }
         cout<<endl;
     }
-}
-
-ll catalan(ll n){
-    return comb(2*n, n)/(n+1); 
-}
-
-//requires fexp.cpp
-ll catalan(ll n, ll mod){ 
-    return (comb(2*n, n)*invmod(n+1,mod))%mod;
 }
 
