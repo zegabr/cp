1c18d2abf753371452f754f58b505a81c89a1560
zegabr <jgsp2@cin.ufpe.br>
2020-06-25 18:52:08 -0300

    refactor template

--- stuff/tmp.cpp
+++ stuff/tmp.cpp
@@ -6,25 +6,14 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#define pb push_back
-#define pf push_front
-#define ppb pop_back
-#define ppf pop_front
 #define db(x) cerr<<#x<<" = "<<x<<endl
-#define ll long long
-#define endl '\n'
 
 
 
-void main2(int test_case){
 
-
-}
 int32_t main(){
     ios::sync_with_stdio(0); cin.tie(0);
-    int t=1;
-    cin>>t;//opa
-    db(t);
-    for(int i=1;i<=t;i++) 
-        main2(t);
+
+
+
 }
