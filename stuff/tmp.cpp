b94b943c93592ab5c8a79b509bfd0df05ccd3fb3
zegabr <jgsp2@cin.ufpe.br>
2020-08-08 10:29:01 -0300

    revert template

--- stuff/tmp.cpp
+++ stuff/tmp.cpp
@@ -6,7 +6,22 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+#define x first
+#define y second
+#define pb push_back
+#define ppb pop_back
+#define pf push_front
+#define ppf pop_front
+#define ll long long
+#define ld long double
+#define ii pair<int,int>
+#define len(v) ((int)v.size())
+#define fa(a,c) for(auto &a : c)
+#define all(k) k.begin(),k.end()
+#define rall(k) k.rbegin(),k.rend()
+
 #define db(x) cerr<<#x<<" = "<<x<<endl
+#define flush cout.flush()
 
 
 
