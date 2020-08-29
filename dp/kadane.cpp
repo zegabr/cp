f282cd00c4f9d420c54a94c9962bda0846e27f55
José Gabriel <jgsp2@cin.ufpe.br>
2020-08-28 22:34:19 -0300

    Update kadane.cpp

--- dp/kadane.cpp
+++ dp/kadane.cpp
@@ -1,11 +1,15 @@
 
-int maxsubarraysum(vector<int> %a){
-	int ans = -inf;
-	vector<int> dp(inf,0);
-	for(int i=1;i<a.size();i++){
-		cin>>a[i];
-		dp[i]=max(a[i],dp[i-1]+a[i]);
-		ans = max(ans, dp[i]);
-	}
-	return ans;
+int maxSubArray(vector<int>& nums) {
+        int currsum = 0;
+        int minsum = 0;
+        int res = nums[0];
+        for(int num : nums){
+            currsum += num;
+            res = max(currsum - minsum, res);
+            minsum = min(minsum, currsum);
+        }
+        return res;
 }
+//supose A is cumulative sum array
+//sum from i to j (i < j) will be the diference between A[j] and A[i-1]
+//to maximiza A[j] - A[i-1] for each j, we want A[i-1] to be the minimum value that appeared in A (before j)
