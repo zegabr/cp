f5d5975d087f8ddcc3d31be44a2f3588f5193fe9
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:11:19 -0300

    Update bellman-ford.cpp

--- graph/bellman-ford.cpp
+++ graph/bellman-ford.cpp
@@ -1,11 +1,13 @@
 
-vector<ii> G[ms];//G[u] contains {w, v}
+vector<pair<int,int>> G[ms];//G[u] contains {w, v}
 
-dis[s]=0; 
-for(int i=0 ; i<V-1 ; i++){
-    for(int u=0 ; u<V ; u++){
-        for(auto &v :    G[u]){
-            dis[v.y] = min(dis[v.y], dis[u]+v.x);
+dis[s]=0;
+for(int i = 0; i < V - 1 ; i++){
+    for(int u = 0; u < V ; u++){
+        for(auto &p : G[u]){
+            int v,w;
+            tie(w,v) = p;
+            dis[v] = min(dis[v], dis[u] + w);
         }
     }
 }
