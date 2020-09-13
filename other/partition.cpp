e0c45012c6987d4955cf1ee753a088d2aa0357d1
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:33:05 -0300

    add randomness

--- other/partition.cpp
+++ other/partition.cpp
@@ -1,7 +1,11 @@
 
 int partition(vector<int> &nums, int l, int r){
     if(l >= r) return l;
+    
+    int randomPos = rand() % (r-l+1) + l;
+    swap(nums[randomPos], nums[r]);
     int pivot = nums[r];
+    
     int i = l; //all numbers before nums[i] are less than pivot
 
     for(int j = l; j < r; j++)
@@ -11,10 +15,3 @@ int partition(vector<int> &nums, int l, int r){
     swap(nums[i], nums[r]); //every element less than pivot was replaced to before nums[i]
     return i;
 }
-
-int randomPartition(vector<int> &nums, int l, int r){ 
-    int n = r-l+1; 
-    int pos = rand() % n; 
-    swap(nums[l + pos], nums[r]); 
-    return partition(nums, l, r); 
-} 
