35bbe6ec708143df853a0c4678216a236f16f907
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:00:49 -0300

    Update dijkstra.cpp

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,22 +1,23 @@
 
-vector<ii> G[ms];//G[u] contains {w, v}
+vector<pair<int,int>> G[ms];//G[u] contains {w, v}
 
 void dijkstra(int S, int T){//O(E logV)
-	priority_queue <ii, vector<ii>, greater<ii>> pq;//contains {dis[u], u}
-	pq.push(ii(0,S));
+	priority_queue <pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;//contains {dis[u], u}
+	pq.push({0,S});
 	dis.assign(ms, inf);
 	dis[S]=0;
 
 	while(pq.size()){
-		int u=pq.top().second; 
-		int disu = pq.top().first;
+		int u,disu;
+		tie(disu, u) = pq.top();
 		pq.pop();
 		if(disu > dis[u])continue;
 		for(auto child : G[u]){
-			int v = child.second,  w = child.first;
-			if(dis[u] + w < dis[v]){           
+			int v,w;
+			tie(w,v) = child;
+			if(dis[v] > dis[u] + w){           
 				dis[v] = dis[u] + w;
-				pq.push(ii(dis[v],v));
+				pq.push({dis[v],v});
 			}
 		}
 	}
