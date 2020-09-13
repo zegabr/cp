4c0fbb709fd8e42fe65c7abafd9570c071588d8b
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:40:32 -0300

    Update bsmin.cpp

--- other/bsmin.cpp
+++ other/bsmin.cpp
@@ -4,10 +4,10 @@ bool check(int m){
 }
 
 int bsmin(int l, int r){
-	while(l<r){
+	while(l < r){
 		int m = l + (r-l)/2;
-		if(check(m)) r=m;
-		else l=m+1;
+		if(check(m)) r = m;
+		else l = m + 1;
 	}
 	return r;
 }
