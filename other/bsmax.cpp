68f3350bdafbfbb00e6ac99192be17e5be768db9
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:41:01 -0300

    Update bsmax.cpp

--- other/bsmax.cpp
+++ other/bsmax.cpp
@@ -4,10 +4,10 @@ bool check(int m){
 }
 
 int bsmax(int l, int r){
-	while(l<r){
+	while(l < r){
 		int m = l + (r-l+1) / 2;
-		if(check(m)) l=m;
-		else r=m-1;
+		if(check(m)) l = m;
+		else r = m - 1;
 	}
 	return l;
 }
