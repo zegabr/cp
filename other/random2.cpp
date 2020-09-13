019431da6120bd48fcda81713626ad2b90b821bd
José Gabriel <jgsp2@cin.ufpe.br>
2020-09-13 02:28:34 -0300

    Update random2.cpp

--- other/random2.cpp
+++ other/random2.cpp
@@ -4,5 +4,8 @@ void reset_seed(){
 }
 
 int randint(int l, int r){//random from l to r inclusive
-    return rand() % (r+1) + l;   
+    return rand() % (r-l+1) + l;   
 }
+//l = 3, r = 10, r-l+1 = 8 == {3,4,5,6,7,8,9,10}
+//                             0 1 2 3 4 5 6 7
+//                       + l = 3 4 5 6 7 8 9 10
