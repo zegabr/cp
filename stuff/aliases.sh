3c07812700ff302a9fdeaebcc7bd2f7e36df5282
zegabr <jgsp2@cin.ufpe.br>
2020-12-20 11:19:22 -0300

    edit compile flags

--- stuff/aliases.sh
+++ stuff/aliases.sh
@@ -1,6 +1,6 @@
 ##COMPETITIVE
 alias m='make && make t'
-alias c='g++ -std=c++17 -O2 -Wshadow -Wall -Wno-unused-result'
+alias c='g++ -std=c++17 -O2 -Wshadow -Wall -Wno-unused-result -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG'
 alias tc='time g++ -std=c++17 -O2'
 alias cw='g++ -std=c++17 -O2 -Wfatal-errors'
 
