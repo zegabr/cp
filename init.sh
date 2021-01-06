ddc8d743ca0cb5ac3e79fe8382f22de680cdb254
Jose <jgsp2@cin.ufpe.br>
2021-01-06 18:13:33 -0300

    update flags

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ cp ./stuff/r.sh ../r.sh
 
 cd .. # go basck to ../cp
 
+touch i
 source r.sh
 echo "cp folder has:"
 ls cp/
--- stuff/aliases.sh
+++ stuff/aliases.sh
@@ -1,6 +1,6 @@
 ##COMPETITIVE
 alias m='make && make t'
-alias c='g++ -std=c++17 -O2 -Wshadow -Wall -Wno-unused-result -g -fsanitize=address -fsanitize=undefined -D_GLIBCXX_DEBUG'
+alias c='g++ -std=c++17 -O2 -Wshadow -Wall -Wno-unused-result -g -fsanitize=address,undefined -D_GLIBCXX_DEBUG -Wno-unused-result -Wno-sign-compare -Wno-char-subscripts'
 alias tc='time g++ -std=c++17 -O2'
 alias cw='g++ -std=c++17 -O2 -Wfatal-errors'
 
