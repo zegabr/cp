d7aeb565af27ccfff590f8748c9934cb3f3ac892
José Gabriel <jgsp2@cin.ufpe.br>
2021-01-16 14:55:54 -0300

    Update tarjan.cpp

--- graph/tarjan.cpp
+++ graph/tarjan.cpp
@@ -1,118 +1,90 @@
-
-//for undirected graph
-//TODO: relearn how it works
-int dfsroot, rootchildren;
-vector<int>num, low, par, cutvertex;
-
-void iscutvertex(int v){
-    cutvertex[v]=true;
-}
-
-void isbridge(int u, int v){
-
-}
-
-void dfspointbridge(int u){
-    low[u]=num[u]=timer++;
-    for(int v : g[u]){
-        if(num[v]==0){
-            par[v]=u;
-            if(u == dfsroot) rootchildren++;
-
-            dfspointbridge(v);
-
-            if(low[v]>=num[u]){
-                iscutvertex(v);
-            }
-            if(low[v]>num[u]){
-                isbridge(u,v);
-            }
-            low[u]=min(low[u], low[v]);
-        }else if(v != par[u]) low[u]=min(low[u],num[v]);
-    }
-}
-
-void findpointbridge(int n=ms){
-    timer=0;
-    num.assign(n+1,0);
-    low.assign(n+1,0);
-    par.assign(n+1,0);
-    cutvertex.assign(n+1,0);
-    for(int i=0;i<n;i++){
-        if(num[i]==0){
-            dfsroot=i; rootchildren=0; dfspointbridge(i);
-            if(rootchildren > 1) iscutvertex(i);
-        }
-    }
-}
-
-
-
-//tarjan SCC aqui
-/*
-TODO: refatorar pra reuzir repeticao de codigo, manter compativel com graph.cpp e ao final disso, copiar 2sat pra esse arquivo.
-todo: manter kosaraju na lib, pois eh mais intuitivo
-*/
-class Solution{
-    vector<vector<int>> g;
-    vector<int> vis;
-    void init(int n){//zera grafo
-        g.assign(n, {});
-        if(vis.size() < n) vis.resize(n);
-    }
-
-    vector<int> num, low;
-    vector<int> S, comp // pra guardar os SCCs
-    int numSCC, dfsId;
-
+// TODO: format and add 2 - sat    
+vector<int> num, low, par, criticalNode; // for SCC, bridges and cut vertexes
+    int dfsId, numSCC, dfsroot, rootchildren;
+    vector<int> S, comp; // pra guardar os SCCs
+    
+ //scc
     void tarjanSCC(int u){// DFS
-        low[u] = num[u] = ++dfsId; // o 1o valor de low e num tem q ser diferente de 0...
+        low[u] = num[u] = dfsId++; 
         S.push_back(u);
-        vis[u] = 1;
+        vis[u] = timer;
         
         for(int v : g[u]){
-            if(num[v]==0) // ...pois usei 0 aqui pra dizer que nao foi visitado ainda
+            if(num[v] == 0) 
                 tarjanSCC(v);
-            if(vis[v]) 
-                low[u]=min(low[u], low[v]);
+            
+            if(vis[v] == timer) 
+                low[u] = min(low[u], low[v]);
         }
         
         if(low[u] == num[u]){
             numSCC++; // aumenta em um a quantidade de SCC
             while(1){
-                int v = S.back(); S.pop_back(); vis[v] = 0;
-                comp[v] = numSCC; // diz q numSCC eh o componente de v
-                if(u == v) break; // acabou o componente
+                int v = S.back();
+                S.pop_back();
+                vis[v] = timer - 1;
+                comp[v] = numSCC;
+                if(u == v) break;
             }
         }
     }
 
-    void findSCCs(int n){
-        dfsId = numSCC = 0;
-        num.assign(n+1,0);//esse valor inicial tem q ser diferente do inicial de dfsId
+    void initTarjan(int n){
+        dfsId = 1;
+        num.assign(n+1,0);
         low.assign(n+1,0);
+    }
+
+    void findSCCs(int n){
+        initTarjan(n);
+        numSCC = 0;
         comp.assign(n+1,0);
-        vis.assign(n+1,0);
         
         for(int i=0;i<n;i++){
-            if(num[i]==0){//nao visitei ainda
+            if(num[i] == 0){//nao visitei ainda
                 tarjanSCC(i);
             }
         }
     }
-	public:
-	
-    int SCC(int V, vector<int> adj[]) {
-        init(V);
-        for(int i = 0; i < V; i++)
-            g[i] = adj[i];
-        
-        numSCC = 0;
-        findSCCs(V);
-        return numSCC;
-    }
-};
 
-// problems for testing:
-//https://practice.geeksforgeeks.org/problems/strongly-connected-components-kosarajus-algo/1#
-//https://binarysearch.com/problems/Edges-that-Disconnect-the-Graph
+
+void setCriticalNode(int v){
+    criticalNode[v] = true;
+}
+
+void setBridge(int u, int v){
+
+}
+void tarjanCritical(int u){
+    low[u] = num[u] = dfsId++;
+    for(int v : g[u]){
+        if(num[v] == 0){
+            par[v] = u;
+            if(u == dfsroot) rootchildren++;
+
+            tarjanCritical(v);
+
+            if(low[v]>=num[u]){
+                setCriticalNode(v);
+            }
+            if(low[v] > num[u]){
+                setBridge(u,v);
+            }
+            low[u] = min(low[u], low[v]);
+        }else if(v != par[u]) low[u] = min(low[u], num[v]);
+    }
+}
+
+    void findCritical(int n){ // find critical nodes and bridges
+    initTarjan(n);
+    par.assign(n+1,0);
+    criticalNode.assign(n+1,0);
+    for(int i=0;i<n;i++){
+        if(num[i] == 0){
+            dfsroot = i; 
+            rootchildren = 0; 
+            tarjanCritical(i);
+            if(rootchildren > 1) setCriticalNode(i);
+        }
+    }
+}
