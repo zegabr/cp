e8232758980e20329e299a5753c960c491fcc1e1
Jose <jgsp2@cin.ufpe.br>
2021-02-14 14:11:09 -0300

    replace bipartite with iterative

--- graph/bipartitecheck.cpp
+++ graph/bipartitecheck.cpp
@@ -1,14 +1,27 @@
 
-vector<bool> color(ms);
-
-bool dfsbicolor(int u){
-    for(int v : g[u]){
-        if(vis[v] < timer){
-            vis[v] = timer;
-            color[v] = !color[u];
-            return dfsbicolor(v);
-        }else if(color[u] == color[v])
-            return false;
+bool isbipartite(vector<vector<int>>& g) {
+    queue<int> q({0});
+    int n = g.size();
+    const int mx = 100; // change this
+    int color[mx] = {0};
+    for(int i = 0; i < n; i++){
+        if(!color[i]){
+            q.push(i);
+            color[i] = 1;
+            while(q.size()){
+                int u = q.front(); q.pop();
+                for(int v : g[u]){
+                    if(color[v] == color[u]){
+                        return false;
+                    }else{
+                        if(!color[v]){    
+                            q.push(v);
+                            color[v] = color[u] == 1 ? 2 : 1;
+                        }
+                    }
+                }
+            }
+        }
     }
     return true;
 }
