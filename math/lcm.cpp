ad2f2034495ec02d7114be7c239ba0f15f9f2ed6
José Gabriel <jgsp2@cin.ufpe.br>
2021-03-20 18:36:59 -0300

    Update lcm.cpp

--- math/lcm.cpp
+++ math/lcm.cpp
@@ -1,4 +1,9 @@
 
+long long gcd(long long a, long long b){
+	if(b == 0) return a;
+	return gcd(b, a % b);
+}
+
 long long lcm(const long long &a, const long long &b) {
 	return (a / __gcd(a, b)) * b; 
 }
