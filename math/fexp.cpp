2a8862a5a3316100fba79ba59cd05bca7e20b9dd
Jose <jgsp2@cin.ufpe.br>
2021-05-11 16:53:03 -0300

    add fexp explanation by example

--- math/fexp.cpp
+++ math/fexp.cpp
@@ -1,4 +1,11 @@
 
+/**
+ * a ^ 6 = a ^ 110 == a ^ (0*2⁰+1*2¹+1*2²)
+ * == a⁰ * a² * a⁴ == a⁰ * a² * (a²)²
+ * So, we keep squaring the base a, 
+ * and multiply it by the answer every time we encounter a 1 bit in the power binary's representation
+ * 
+ * */
 long long fexp(long long a, long long b) {//O(logb)
 	long long ans = 1;
 	while(b) {
