4df791ee7b6a6d1a1dee6a6b7ad73938c8928c4d
zegabr <jgsp2@cin.ufpe.br>
2021-10-06 11:43:56 -0300

    create graph classes

--- graph/grid.cpp
+++
@@ -1,3 +0,0 @@
-
-const vector<vector<int>> kingsMoves = {{-1,0},{0,1},{1,0},{0,-1},{-1,1},{1,1},{1,-1},{-1,-1}};//4 adj + 4 diags
-const vector<vector<int>> knightsMoves = {{-2,-1},{-2,1},{-1,-2},{-1,2},{1,-2},{1,2},{2,-1},{2,1}};
---
+++ graph/weightedGraph.h
@@ -0,0 +1,28 @@
+
+#include<vector>
+class WeightedGraph{
+    public:
+    // vis[u]<timer ==> u has not been visited
+    // vis[u]==timer ==> u has been visited
+    // timer++ ==> unvisit every node
+    int timer = 0;
+    static const int ms = 300000;
+    static const int inf = 1e8+5;
+    std::vector<int> dis; // distance
+    std::vector<int> vis; // visited
+    std::vector<std::vector<std::pair<int,int>>> G;
+
+    WeightedGraph(int n = ms, bool usingInverseGraph = false){
+        G.assign(n, {});
+        dis.assign(n,inf);
+        if(vis.size() < n) vis.resize(n);
+        timer++;
+    }
+
+    void add(int u, int v, int weigth ,bool bidirectional = false){ // USE 0 INDEXED VERTICES
+        G[u].push_back({weigth,v});
+        if(bidirectional){
+            G[v].push_back({weigth,u});
+        }
+    }
+};
